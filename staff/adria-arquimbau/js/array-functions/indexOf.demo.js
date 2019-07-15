08fdf9b15e3c375b8428a4b1d474fadd822e90a3
Adria Arquimbau <adria.arki@gmail.com>
2019-07-15 22:57:06 +0200

    for each and indexOf

--- staff/adria-arquimbau/js/array-functions/indexOf.demo.js
+++ staff/adria-arquimbau/js/array-functions/indexOf.demo.js
@@ -1,14 +1,22 @@
 console.log('DEMO: indexOf');
 
-var array = [2, 9, 9];
+var array = [2, 9, 'verde', 'blanco'];
 console.log('array', array);
 
-console.log('indexOf');
+console.log('indexOf de 2');
 var result = array.indexOf(2);
 console.log(result, 'expected 0');
 
-array.indexOf(2);     // 0
-array.indexOf(7);     // -1
-array.indexOf(9, 2);  // 2
-array.indexOf(2, -1); // -1
-array.indexOf(2, -3); // 0
+console.log('indexOf de 7');
+result = array.indexOf(7);
+console.log(result, 'expected -1');
+
+console.log('indexOf de blanco');
+result = array.indexOf('blanco');
+console.log(result, 'expected 3');
+
+console.log('indexOf de naranja');
+result = array.indexOf('naranja');
+console.log(result, 'expected -1');
+
+
