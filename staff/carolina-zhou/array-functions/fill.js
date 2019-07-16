d00b02c59469808271bce86626948d85817e2286
CarolinaZLin <carolina.z.lin@gmail.com>
2019-07-16 11:58:57 +0200

    rename files

--- staff/carolina-zhou/array-functions/fill.js
+++ staff/carolina-zhou/array-functions/fill.js
@@ -1,28 +1,25 @@
 function fill(array, value, start, end) {
     var i=0;
     var j=0;
-
+    
     if (end){
         j=start;
         for (j; j<end; j++){
-            array[j]=value;
+          array[j]=value;
         }
-        //  debugger
-        return  array[j]=value;
-    } 
-
-    else if (start){
+        return array;
+        
+    } else if (start){
         i=start-1;
         for (i; i<array.length-1; i++){
             array[i]=value;
         }
-        return  array[i]=value;
+        return  array;
 
-    } else 
-        {
+    } else {
         for (var i=0; i<array.length-1; i++){
             array[i]=value;
         }
-        return  array[i]=value;
+        return  array;
     }
 }
--- staff/carolina-zhou/array-functions/for-each.js
+++ staff/carolina-zhou/array-functions/for-each.js
@@ -3,3 +3,9 @@ function forEach(array){
     console.log(`The element of position ${[i]} is: ${array[i]}`)
   }
 }
+
+function forEach2(array, expression){
+  for(i=0; i<array.length; i++){
+    expression(array[i])
+  }
+}
