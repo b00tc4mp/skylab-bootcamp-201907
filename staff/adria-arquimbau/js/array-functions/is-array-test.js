4c14916b3d04895aca5bd1c12331dace9125b50c
Adria Arquimbau <adria.arki@gmail.com>
2019-07-16 16:34:07 +0200

    update

---
+++ staff/adria-arquimbau/js/array-functions/is-array-test.js
@@ -0,0 +1,11 @@
+console.log('DEMO: isArray');
+
+var array = [1,2,3];
+
+var result = isArray(array);
+check(result, true);
+
+array = "manzana" 
+
+var result = isArray(array);
+check(result, false);
