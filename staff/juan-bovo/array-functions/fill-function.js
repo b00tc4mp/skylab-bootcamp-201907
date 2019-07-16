c1238a2ea6236909e1bc3f9aed2e0b2cd2cca5f9
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-07-16 13:09:23 +0200

    updating array functions

---
+++ staff/juan-bovo/array-functions/fill-function.js
@@ -0,0 +1,23 @@
+// array1 = [1, 2, 3, 4];
+
+function fill(arr, value, start, end) {
+    var newArray = arr;
+    if (end) {
+        for (var i = start; i < end; i++) {
+            newArray[i] = value;
+        }
+    } else if (start) {
+        for (var i = start; i < arr.length; i++){
+            newArray[i] = value;
+        }
+    } else {
+        for (i = 0; i< arr.length; i++){
+            newArray[i] = value;
+        }
+    }
+    return newArray;
+}
+
+// console.log(fill(array1, 0, 2, 4)); // expected output: [1, 2, 0, 0]
+// console.log(fill(array1, 5, 1)); // expected output: [1, 5, 5, 5]
+// console.log(fill(array1, 6)); // expected output: [6, 6, 6, 6]
