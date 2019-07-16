51c3b1edb6cc1350cf99473b0e4014d8651a0efd
CarolinaZLin <carolina.z.lin@gmail.com>
2019-07-16 08:38:05 +0200

    add more array-functions

--- staff/carolina-zhou/array-functions/reduce.js
+++ staff/carolina-zhou/array-functions/reduce.js
@@ -9,12 +9,7 @@ function reduce(array, reducer, initialValue) {
     }
       
     for (var i = start; i < array.length; i++) {
-        accumulator = reducer(accumulator, arr[i])
+        accumulator = reducer(accumulator, array[i])
     }
     return accumulator
   }
-  
-  
-  reduce(arr, function(accumulator, value) {
-      return accumulator + value;
-  })
