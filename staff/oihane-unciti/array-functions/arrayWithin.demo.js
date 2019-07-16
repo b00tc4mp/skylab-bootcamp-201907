fc97c8fac0107809b45dc4a212d2801721b6abcf
Oihane <oihaneuncitig@gmail.com>
2019-07-16 09:00:58 +0200

    array functions

---
+++ staff/oihane-unciti/array-functions/arrayWithin.demo.js
@@ -0,0 +1,9 @@
+console.log('DEMO: arrayWithin');
+
+var array1 = [1,2,3];
+
+
+console.log('arrayWithin');
+var result = arrayWithin(array1, 2 , 1);
+console.log(result, "expected: [1,2,1]" );
+
---
+++ staff/oihane-unciti/array-functions/from.demo.js
@@ -0,0 +1,8 @@
+console.log('DEMO: from');
+
+var item = "manzana";
+console.log('array');
+
+console.log('from');
+var result = from(item);
+console.log(result, "expected: [\"m\" \"a\" \"n\" \"z\" \"a\" \"n\" \"a\"] " );
