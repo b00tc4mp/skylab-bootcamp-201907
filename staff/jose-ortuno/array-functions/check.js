17ca71246349f5fb2c123b0017464fd37760ae0e
xossse <xossse@MacBook-Pro-de-xossse.local>
2019-07-17 18:11:45 +0200

    reeeeee update

--- staff/jose-ortuno/array-functions/check.js
+++ staff/jose-ortuno/array-functions/check.js
@@ -1,5 +1,23 @@
-function check (result, expected) {
+// V1
+// function check (result, expected) {
+//     if (result.toString() !== expected.toString()) {
+//         console.error('error: result (' + result + ') does not match expected value (' + expected + ')');
+//     };
+// };
+
+// V2
+
+/**
+ * Checks wether a testing result matches an expected value.
+ * In case of matching values, it does nothing. Otherwise shows an error explaining the explaining te unmatching situation.
+ * @param result - The value to value
+ * @param expected - The expected value to check the result against.
+ * 
+ * @version 3.0.0
+ */
+
+ function check (result, expected) {
     if (result.toString() !== expected.toString()) {
         console.error('error: result (' + result + ') does not match expected value (' + expected + ')');
     };
-};
+}
--- staff/jose-ortuno/array-functions/flat.test.js
+++ staff/jose-ortuno/array-functions/flat.test.js
@@ -1,52 +1,55 @@
 console.log('TEST flat');
 
-var array = [1, 2, 3, ['a', 'b', 'c', [true, false, [undefined, null]]]];
+var array = [1, 2, 3, ['a', 'b', 'c', [true, false, [undefined, null, [{}, function () { }, [NaN]]]]]];
 
 // case: default
-debugger
+
 var result = flat(array);
-check(result, [1, 2, 3, 'a', 'b', 'c', [true, false, [undefined, null]]]);
+check(result, [1, 2, 3, 'a', 'b', 'c', [true, false, [undefined, null, [{}, function () { }, [NaN]]]]]);
 
 // case: depth 2
 
 var result = flat(array, 2);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, [undefined, null]]);
+check(result, [1, 2, 3, 'a', 'b', 'c', true, false, [undefined, null, [{}, function () { }, [NaN]]]]);
 
 // case: depth 3
 
 var result = flat(array, 3);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null]);
+check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null, [{}, function () { }, [NaN]]]);
 
 // case: random depth (1...10)
 
 // var depth = Math.floor(Math.random() * 10) + 1;
 
 var result = flat(array, 4);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null]);
+check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null, {}, function () { }, [NaN]]);
+
+var result = flat(array, 5);
+check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null, {}, function () { }, NaN]);
 
 // case: no array
 
 try {
-   flat();
+    flat();
 } catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'undefined is not an array');
+    check(error instanceof TypeError, true);
+    check(error.message, 'undefined is not an array');
 }
 
 // case: string as array
 
 try {
-   flat('array');
+    flat('array');
 } catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'array is not an array');
+    check(error instanceof TypeError, true);
+    check(error.message, 'array is not an array');
 }
 
 // case: string as array
 
 try {
-   flat(true);
+    flat(true);
 } catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'true is not an array');
+    check(error instanceof TypeError, true);
+    check(error.message, 'true is not an array');
 }
