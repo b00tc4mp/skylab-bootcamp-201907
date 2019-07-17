be7da6394a1b005d9ae6033bcfdf674f9983fec6
Oihane <oihaneuncitig@gmail.com>
2019-07-17 18:04:48 +0200

    update

--- staff/oihane-unciti/array-functions/flat.test.js
+++ staff/oihane-unciti/array-functions/flat.test.js
@@ -1,52 +1,20 @@
-console.log('TEST flat');
+console.log('TEST map');
 
-var array = [1, 2, 3, ['a', 'b', 'c', [true, false, [undefined, null]]]];
+var array = [1, 2, 3];
 
-// case: default
+var coeficient = 10;
 
-var result = flat(array);
-check(result, [1, 2, 3, 'a', 'b', 'c', [true, false, [undefined, null]]]);
+var result = map(array, function(value) { return value * coeficient; });
+check(result, [10, 20, 30]);
 
-// case: depth 2
+array = ['1', '2', '3'];
 
-var result = flat(array, 2);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, [undefined, null]]);
+var result = map(array, function(value) { return '<' + value + '>'; });
+check(result, ["<1>", "<2>", "<3>"]);
 
-// case: depth 3
+array = [1, 2, 3];
 
-var result = flat(array, 3);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null]);
-
-// case: random depth (1...10)
-
-// var depth = Math.floor(Math.random() * 10) + 1;
-
-var result = flat(array, 4);
-check(result, [1, 2, 3, 'a', 'b', 'c', true, false, undefined, null]);
-
-// case: no array
-
-try {
-   flat();
-} catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'undefined is not an array');
-}
-
-// case: string as array
-
-try {
-   flat('array');
-} catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'array is not an array');
-}
-
-// case: string as array
-
-try {
-   flat(true);
-} catch (error) {
-   check(error instanceof TypeError, true);
-   check(error.message, 'true is not an array');
-}
+var result = map(array, function(value,  index, array) { 
+    return value + '-' + index + '-' + array; 
+});
+check(result, ["1-0-1,2,3", "2-1-1,2,3", "3-2-1,2,3"]);
