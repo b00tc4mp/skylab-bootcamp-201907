0aa4e6472ee15dff2af5044fe00b147868b25e4c
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-07-18 18:24:44 +0200

    update directories estructure

--- staff/jaume-villarreal/array-functions/0/js/map.js
+++
@@ -1,7 +0,0 @@
-function map(array , expression){
-  var result=[];
-  for(var i = 0; i < array.length ; i++){
-     result[i] = expression(array[i]);
-  }
-  return result;
-}
--- staff/jaume-villarreal/array-functions/0/js/map.test.js
+++
@@ -1,9 +0,0 @@
-console.log('TEST: map');
-
-array = [1 , 2 , 3 , 4];
-
-var result1 = map(array , function(value){
-    return value * 2;
-});
-
-console.log(result1);
