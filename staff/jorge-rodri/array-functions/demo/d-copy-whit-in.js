ef26bd94e2fca1b564501897e01a5b80ac664c68
Jorge <jorgegrancanaria@gmail.com>
2019-07-18 17:44:29 +0200

    modified files

--- staff/jorge-rodri/array-functions/demo/d-copy-whit-in.js
+++ staff/jorge-rodri/array-functions/demo/d-copy-whit-in.js
@@ -4,7 +4,7 @@
 function copyWhitinD(target,start,end,arr){
     if(target==undefined||start==undefined||end==undefined||arr==undefined)throw ReferenceError("Missing params.");
     if(!(arr instanceof Array))throw TypeError("The type of param arr is not Array");
-    if(!(start instanceof Number)||!(end instanceof Number))throw TypeError("'start' or 'end' are not params.");
+    if(!(typeof start=="number")&&!(typeof end=="number"))throw TypeError("'start' or 'end' are not params.");
     var res=[];
     var count=0;
     for(var i=start;i<end+1;i++){
--- staff/jorge-rodri/array-functions/test/test-copyWhitin.js
+++ staff/jorge-rodri/array-functions/test/test-copyWhitin.js
@@ -5,4 +5,10 @@ suite('copyWhitInD', function(){
         check(error instanceof ReferenceError, true);
         check(error.message, "Missing params.");
     })
+    test("'start' or 'end' are not a number", function(){
+        copyWhitinD(5,"a","b",[0,1]);
+    }, function(error){
+        check(error instanceof TypeError, true);
+        check(error.message, "'start' or 'end' are not params.")
+    })
 })
