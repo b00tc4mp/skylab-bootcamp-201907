cd84ae3702ec5432805c4a12bf43152780c9a849
Adria Arquimbau <adria.arki@gmail.com>
2019-07-19 08:01:00 +0200

    update

--- staff/adria-arquimbau/js/array-functions/arrayConcat.js
+++ staff/adria-arquimbau/js/array-functions/arrayConcat.js
@@ -2,11 +2,11 @@ function arrayConcat(array1, array2) {
     var arraySum = [];
 
     for(var i=0; i<array1.length; i++){
-        arraySum.push(array1[i])
+        arraySum.push(array1[i]);
     }
 
     for(var i=0; i<array2.length; i++){
-        arraySum.push(array2[i])
+        arraySum.push(array2[i]);
     }
     
 
