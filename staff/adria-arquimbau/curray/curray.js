173f7accb63185a3b99da4f223ad522dab19acc6
Adria Arquimbau <adria.arki@gmail.com>
2019-07-20 16:44:36 +0200

    update

--- staff/adria-arquimbau/curray/curray.js
+++ staff/adria-arquimbau/curray/curray.js
@@ -3,16 +3,16 @@
 /**
  * DIY Array.
  * 
- * 
+ *  
  */
 function Curray() {
     this.length = 0;
-''
+
     if (arguments.length === 1) {
         this.length = arguments[0];
     } else if (arguments.length > 1) {
         for (var i = 0; i < arguments.length; i++) {
-              this[i] = arguments[i];
+            this[i] = arguments[i];
             // this.push(arguments[i]); // TRY not to depend on push here.
         }
         this.length = arguments.length;
@@ -25,7 +25,7 @@ Curray.prototype.push = function (element) {
     return this.length;
 };
 
-Curray.prototype.pop = function() {
+Curray.prototype.pop = function () {
     var last = this[--this.length];
 
     delete this[this.length];
@@ -33,7 +33,7 @@ Curray.prototype.pop = function() {
     return last;
 };
 
-Curray.prototype.forEach = function(expression) {
+Curray.prototype.forEach = function (expression) {
     if (arguments.length === 0) throw TypeError('missing argument 0 when calling function forEach');
 
     if (!(expression instanceof Function)) throw TypeError(expression + ' is not a function');
@@ -42,45 +42,141 @@ Curray.prototype.forEach = function(expression) {
         expression(this[i], i, this);
 };
 
-Curray.prototype.concat = function(curray2) {
+Curray.prototype.concat = function (curray2) {
+
     var concat = new Curray;
-    for(var i=0; i<this.length; i++){
+
+    for (var i = 0; i < this.length; i++) {
         concat.push(this[i]);
     }
-    for(var i=0; i<curray2.length; i++){
+    for (var i = 0; i < curray2.length; i++) {
         concat.push(curray2[i]);
     }
     return concat;
 };
 
-Curray.prototype.flat = function(depth) {
+Curray.prototype.flat = function (depth) {
 
-    depth = typeof depth === 'undefined'? 1: depth;
-    depth = depth < 0? 0:depth;
+    depth = typeof depth === 'undefined' ? 1 : depth;
+    depth = depth < 0 ? 0 : depth;
 
     var result = [];
 
-    for (var i = 0; i < array.length; i++) {
-        var element = array[i];
+    for (var i = 0; i < this.length; i++) {
+        var element = this[i];
 
-            if (element instanceof Array && depth > 0) {
-                var arr = flat(element, depth -1);
+        if (element instanceof Curray && depth > 0) {
+            var arr = this[i].flat(depth - 1, element);
 
-                for (var j = 0 ;j < array.length; j++) 
-                result.push(array[j]);
+            for (var j = 0; j < arr.length; j++) result.push(arr[j]);
+        } else result.push(element);
+    }
 
-            } else result.push(element);
-            
-        
-    } 
+    return result;
+}
 
-    Curray.prototype.copyWithin = function(target, start) {
-/*         if (arguments.length === 0) throw TypeError('missing argument 0 when calling function copyWithin');
-   
-        if (!(curray instanceof Curray)) throw TypeError("1 is not an array");
- */   
+Curray.prototype.copyWithin = function (target, start) {
+    if (arguments.length === 0) throw TypeError('missing argument 0 when calling function copyWithin');
 
+    // if (!(curray instanceof Curray)) throw TypeError("1 is not an array");
 
-       return this[target] = this[start];
-    };
+    this[target] = this[start];
+    return this;
 };
+
+Curray.prototype.map = function (expression) {
+
+    if (arguments.length === 0) throw TypeError('missing argument 0 when calling function map');
+
+    var result = this;
+
+    for (var i = 0; i < this.length; i++)
+        result[i] = expression(this[i], i, this);
+
+    return result;
+}
+
+
+Curray.prototype.arrayOf = function () {
+    var curray = [];
+
+    for (var i = 0; i < this.length; i++) {
+
+        curray.push(this[i]);
+
+    }
+
+    return curray;
+}
+
+Curray.prototype.every = function (expression) {
+
+    var result = true;
+
+    for (var i = 0; i < this.length; i++) {
+
+        if (expression(this[i]) && result == true) {
+            return true;
+        } else {
+            return false;
+        }
+        return result;
+    }
+
+    /*
+    Curray.prototype.fill = function (value, start, end) {
+            var result = [];
+            if (start === undefined) {
+                start = 0;
+            }
+            if (end === undefined) {
+                end = this.length;
+            }
+            for (var i = start; i < end; i++) {
+                this[i] = val;
+            }
+            return result;
+            */
+}
+
+
+Curray.prototype.fill = function (value, start, end) {
+
+    if (arguments.length === 0) throw TypeError('missing argument 0 when calling function fill');
+
+    var i = 0;
+    var j = 0;
+
+    if (end) {
+
+        j = start;
+
+        for (j; j < end; j++) {
+
+            this[j] = value;
+
+        }
+        return this;
+
+    } else if (start) {
+
+        i = start;
+
+        for (i; i < this.length - 1; i++) {
+            this[i] = value;
+        }
+
+        return this;
+
+    } else {
+        for (var i = 0; i < this.length - 1; i++) {
+
+            this[i] = value;
+
+        }
+
+        return this;
+    }
+}
+
+
--- staff/adria-arquimbau/curray/curray.spec.js
+++ staff/adria-arquimbau/curray/curray.spec.js
@@ -41,61 +41,163 @@ describe('Curray', function () {
             ]);
         });
 
-        it('should fail on no arguments', function() {
+        it('should fail on no arguments', function () {
             var curray = new Curray();
 
-            expect(function() {
+            expect(function () {
                 curray.forEach();
             }).toThrowError(TypeError, 'missing argument 0 when calling function forEach');
         });
     });
 
-    describe('arrayConcat', function() {
-        it('should concat 2 elements', function() {
-            var curray = new Curray (1 ,2, 3);
-            var curray2 = new Curray (4, 5, 6);
- 
+    describe('concat', function () {
+        it('should concat 2 elements', function () {
+            var curray = new Curray(1, 2, 3);
+
+            var curray2 = new Curray(4, 5, 6);
+
             var result = curray.concat(curray2);
             expect(result).toEqual[1, 2, 3, 4, 5, 6];
         });
     });
 
-    describe('flat', function() {
-            it('should flatten a curray', function() {
-                
-                    var curray1 = new Curray;
 
-                curray1 = [1, 2, 3, ['a', 'b', 'c', [true, false]]];
+    describe('flat', function () {
+        it('should flatten a curray/one level', function () {
+            var curray1 = new Curray(1, 2, 3, new Curray("a", "b", "c", new Curray(true, false)));
 
-
-                var result = curray1.flat();
-        
-                expect(result).toEqual([1, 2, 3, 'a', 'b', 'c', [true, false]]);
-            });
-
-            it('should be flatten a curray/two level', function() {
-                
-                var curray2 = new Curray;
-
-            curray2 = [1, 2, 3, ['a', 'b', 'c', [true, false, [undefined, null]]]];
-            var result = curray2.flat(2);
-            expect(result).toEqual([1, 2, 3, 'a', 'b', 'c', true, false, [undefined, null]]);
-            });
+            var result = curray1.flat();
+            expect(result).toEqual([1, 2, 3, 'a', 'b', 'c', new Curray(true, false)]);
+        });
+        it('should flatten a curray/two level', function () {
+            var curray1 = new Curray;
+            curray1 = ([1, 2, 3, ['a', 'b', 'c', [true, false]]]);
+            var result = curray1.flat(2);
+            expect(result).toEqual([1, 2, 3, 'a', 'b', 'c', true, false]);
+        });
     });
 
-    describe('copyWithin', function() {
-        it('should change the target and give a new arrays', function() {
-            
-            var curray1 = new Curray;
-            curray1 = ([1,2,3]);
+    describe('copyWithin', function () {
+        it('should copy part of the curray whouth changing the lenght', function () {
+            var curray1 = new Curray();
 
-            var result = arrayWithin(array1, 2 , 1);
- 
-            var result = curray1.copyWithin(2,1);
-            expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+            curray1 = ([1, 2, 3]);
+
+            var result = curray1.copyWithin(1, 2);
+            expect(result).toEqual([1, 3, 3]);
+        });
+        it('should fail on no arguments', function () {
+            var curray1 = new Curray();
+
+            expect(function () {
+                curray1.copyWithin();
+            }).toThrowError(TypeError, 'missing argument 0 when calling function copyWithin');
         });
     });
 
 
+    describe('map', function () {
+        it('multiply by 10 all items', function () {
+            var curray1 = new Curray(1, 2, 3);
+
+
+            var result = curray1.map(function (value) {
+                return value * 10;
+            });
+            result = Array.from(result)
+            expect(result).toEqual([10, 20, 30]);
+        });
+
+        it('wrap each element between <>', function () {
+            var curray1 = new Curray('1', '2', '3');
+
+            var result = [];
+
+            result = curray1.map(function (value) {
+                return '<' + value + '>';
+            });
+
+
+            result = Array.from(result)
+            expect(result).toEqual(["<1>", "<2>", "<3>"]);
+        });
+
+        it('should fail on no arguments', function () {
+            var curray1 = new Curray();
+
+            expect(function () {
+                curray1.map();
+            }).toThrowError(TypeError, 'missing argument 0 when calling function map');
+        });
+
+        //  it('concatenate value-index-array', function () {
+        //      var array = [1, 2, 3];
+
+        //      var result = map(array, function (value, index, array) {
+        //          return value + '-' + index + '-' + array;
+        //      });
+
+        //      expect(result, ["1-0-1,2,3", "2-1-1,2,3", "3-2-1,2,3"]);
+        //  })
+    });
+
+    describe('arrayOf', function () {
+        it('should introduce the arguments into a new curray', function () {
+            var curray = new Curray(1, 2, 3, 'hello', {}, NaN);
+
+            var result = (curray).arrayOf();
+            expect(Array.from(result)).toEqual([1, 2, 3, 'hello', {}, NaN]);
+        })
+    });
+
+    describe('every', function () {
+        it('should check all elements of the curray pass the condition of the function done', function () {
+            var curray = new Curray(1, 30, 29, 10, 13);
+
+            var result = (curray).every(function (currentValue) {
+                return currentValue > 30;
+            });
+            expect(result).toBe(false);
+        })
+    });
+
+    describe('fill', function () {
+        it('should fill all the items with the input introduced to and from the desired start index', function () {
+            var curray = new Curray(1, 2, 3, 4, 5);
+
+            var result = curray.fill(5, 1);
+
+
+            result = Array.from(result);
+            expect(result).toEqual([1, 5, 5, 5, 5]);
+
+        });
+
+        it('fill all the items with the input passed to a fill from the start index you passed', function () {
+
+            var curray = new Curray(1, 2, 3, 4, 5);
+
+            var result = curray.fill(5);
+            var expected = [5, 5, 5, 5, 5];
+
+            //we do a for loop in order to compare the items of the arrays
+            //because with expectArrays we can't compare arrays with Objects(result is an Object)
+            var arrResult = Array.from(result)
+            expect(arrResult).toEqual(expected);
+        });
+
+        /*
+        describe('fill',function(){
+            it('should fill some elements from an appointed start point to an appointed end point (zero-index location) of the curray', function(){
+                var curray1 = new Curray('a', 'b', 'c', 'd', 'f','g');
+
+                var result = curray1.fill("z", 2);
+
+                expect(Array.from(result)).toEqual(['a', 'b', 'z', 'z', 'z', 'z']);
+            })
+        });
+        */
+
+    });
 
 });
