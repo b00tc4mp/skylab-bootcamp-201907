df4ea5069e2d78b9576de913d3c2016fbc15c4a8
Anna <daeneris88@gmail.com>
2019-07-22 14:03:26 +0200

    curray

--- staff/anna-nolla/curray/curray.js
+++ staff/anna-nolla/curray/curray.js
@@ -3,7 +3,7 @@
 /**
  * DIY Array.
  * 
- * 
+ *  
  */
 function Curray() {
     this.length = 0;
@@ -13,36 +13,129 @@ function Curray() {
     } else if (arguments.length > 1) {
         for (var i = 0; i < arguments.length; i++) {
             this[i] = arguments[i];
-            
             // this.push(arguments[i]); // TRY not to depend on push here.
         }
         this.length = arguments.length;
     }
 }
 
-Curray.prototype.push = function () {
-    for (var i = 0; i < arguments.length; i++){
-        this[this.length++] = arguments[i];
-    }
-    
+Curray.prototype.push = function (element) {
+    this[this.length++] = element;
+
     return this.length;
 };
 
-Curray.prototype.pop = function() {
+Curray.prototype.pop = function () {
     var last = this[--this.length];
 
     delete this[this.length];
     return last;
 };
 
-Curray.prototype.forEach = function(expression) {
-
-    if (arguments.length === 0) throw TypeError('missing argument 0 when calling function forEach');
+Curray.prototype.forEach = function (expression) {
+    if (arguments.length === 0) throw TypeError('missing argument ');
 
     if (!(expression instanceof Function)) throw TypeError(expression + ' is not a function');
 
     for (var i = 0; i < this.length; i++)
         expression(this[i], i, this);
+};
+
+Curray.prototype.concat = function (curray2) {
+
+    var concat = new Curray;
+
+    for (var i = 0; i < this.length; i++) {
+        concat.push(this[i]);
+    }
+    for (var i = 0; i < curray2.length; i++) {
+        concat.push(curray2[i]);
+    }
+    return concat;
+};
+
+Curray.prototype.flat = function (depth) {
+    depth = typeof depth === 'undefined' ? 1 : depth;
+    depth = depth < 0 ? 0 : depth;
+    var result = [];
+
+    for (var i = 0; i < this.length; i++) {
+        var element = this[i];
+
+        if (element instanceof Curray && depth > 0) {
+            var arr = this[i].flat(depth - 1, element);
+
+            for (var j = 0; j < arr.length; j++) result.push(arr[j]);
+        } else result.push(element);
+    }
+
+    return result;
+};
+
+Curray.prototype.copyWithin = function (target, start) {
+    if (arguments.length === 0) throw TypeError('missing argument');
+
+    this[target] = this[start];
+    return this;
+};
+
+Curray.prototype.map = function (expression) {
+
+    if (arguments.length === 0) throw TypeError('missing argument');
+    var result = this;
+
+    for (var i = 0; i < this.length; i++)
+        result[i] = expression(this[i], i, this);
+
+    return result;
+};
+
+Curray.prototype.arrayOf = function () {
+    var curray = [];
+
+    for (var i = 0; i < this.length; i++) {
+        curray.push(this[i]);
+    }
+    return curray;
+};
+
+Curray.prototype.every = function (expression) {
+    var result = true;
+
+    for (var i = 0; i < this.length; i++) {
+        if (expression(this[i]) && result == true) {
+            return true;
+        } else {
+            return false;
+        }
+        return result;
+    }
+};
+
+Curray.prototype.fill = function (value, start, end) {
+
+    if (arguments.length === 0) throw TypeError('missing argument');
+    var i = 0;
+    var j = 0;
+
+    if (end) {
+        j = start;
+        for (j; j < end; j++) {
+            this[j] = value;
+        }
+        return this;
+    } else if (start) {
+        i = start;
+        for (i; i < this.length - 1; i++) {
+            this[i] = value;
+        }
+        return this;
+    } else {
+        for (var i = 0; i < this.length - 1; i++) {
+            this[i] = value;
+        }
+        return this;
+    }
 };
 
 Curray.prototype.join = function(separador){
@@ -62,7 +155,6 @@ Curray.prototype.join = function(separador){
         if(this[i] === null || this[i] === undefined){
             arr[i] = " ";
         }
-
         if(i === (this.length -1)){
             count += this[i];
         }
@@ -70,19 +162,63 @@ Curray.prototype.join = function(separador){
             count += this[i] + separador;
         }
     }
-
     return (count);
 };
 
-Curray.prototype.concat = function(arguments) {
-    var curray = [];
+Curray.prototype.some = function(curr, expresion){
+    if(!(this instanceof Curray)) {
+        throw TypeError("This is not a Curray");
+    }
 
-    for (var i = 0; i < arguments.length; i++) {
-        for (j = 0; j < arguments[i].length; j++) {
-            curray += this[i][j];
+    for(var i = 0; i < curr.length; i++){
+        if(expresion(curr[i])){
+            return true;
+        }
+        else{
+            continue;
         }
     }
-    curray = curray.split("");
-
-    return (curray);
+    return false;
 };
+
+Curray.prototype.reduce = function(curr, expresion){
+    var result = null;
+
+    for(var i = 0; i < curr.length; i++){
+        result += expresion(curr[i]);
+    }
+    return result;
+}
+
+function suma(value){
+    return value * 10;
+};
+
+Curray.prototype.filter = function(curr, expresion){
+    var result = new Curray;
+    if(!(curr instanceof Curray)) {
+        throw TypeError("This is not an array");
+    }
+    for(var i = 0; i < curr.length; i++){
+        if(expresion(curr[i])){
+            result.push(curr[i]);
+        }
+    }
+    return result;
+}
+
+Curray.prototype.from = function(curr, expresion){
+    var result = new Curray;
+ 
+    if (expresion === undefined){
+        for(var i = 0; i < curr.length; i++){
+            result.push(curr[i]);
+        }
+        return result;
+    }
+
+    for(var i = 0; i < curr.length; i++){
+        result.push(expresion(curr[i]));
+    }
+    return result;
+}
--- staff/anna-nolla/curray/curray.test.js
+++ staff/anna-nolla/curray/curray.test.js
@@ -15,7 +15,7 @@ describe('Curray', function() {
             var curray = new Curray(1,2);
             var result = curray.push(3,4);
 
-            expect(result).toBe(4);
+            expect(result).toBe(3);
         });
     });
 
@@ -60,25 +60,91 @@ describe('Curray', function() {
             var curray = new Curray(1,2,3,4,5,6,7,8,9);
             curray = curray.join( );
 
-            expect(curray, "1,2,3,4,5,6,7,8,9");
+            expect(curray).toBe("1,2,3,4,5,6,7,8,9");
         });
         it("should convert the numb to string", function() {
             var curray = new Curray(1,2,3,4,5);
             curray = curray.join(0);
 
-            expect(curray, "102030405");
+            expect(curray).toBe("102030405");
         });
     });
 
     describe("concat", function(){
         it("should join them all in one", function(){
-            var curray = new Curray(1,2,3);
-            var curr1 = new Curray(4,5,6);
-            var curr2 = new Curray("a", "b", "c");
-
-            var result = result.concat(curray, curr1, curr2);
+            var curray = new Curray(1, 2, 3);
+            var curr1 = new Curray(4, 5, 6);
+            
+            var result = curray.concat(curr1);
             console.log(result);
+            expect(result).toEqual(new Curray(1,2,3,4,5,6));
         });
     });
 
+    describe("some", function(){
+        it("should show true is any element is in the parameter", function(){
+            var curray = new Curray(1, 2, 3, 4, 6, 7);
+
+            function mayor (value){
+                return value <=2;
+             }
+            
+            var result = curray.some(curray, mayor);
+            expect(result).toBe(true);
+        });
+        it("should show false due to no element in the parameter", function(){
+            var curray = new Curray(1, 2, 3, 4, 6, 7);
+
+            function mayor (value){
+                return value >= 10;
+             }
+            
+            var result = curray.some(curray, mayor);
+            expect(result).toBe(false);
+        });
+    });
+
+    describe("some", function(){
+        it("should return the the sum of the values *10", function(){
+            var curray = new Curray(1, 2, 3, 4, 5);
+
+            function suma(value){
+                return value * 10;
+            }
+
+            curray = curray.reduce(curray, suma);
+            expect(curray).toBe(150);
+        });
+    });
+    describe("filter", function(){
+        it("should return the values lower than 5", function(){
+            var curray = new Curray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            
+            function mayor (value){
+                return value <= 5;
+             }
+            
+            var result = curray.filter(curray, mayor);
+            expect(result).toEqual(new Curray(1,2,3,4,5));
+        });
+    });
+
+    describe("from", function(){
+        it("should return the values lower than 5", function(){
+            var curray = new Curray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            
+            function suma(value){
+                return value * 10;
+            }
+            
+            var result = curray.from(5, suma);
+            expect(result).toEqual(new Curray(60, 70, 80, 90));
+        });
+        
+    });
+
+
+
 });
+
+
