0845a1a236f28482fa2930cce8d69089b4ed001c
Oihane <oihaneuncitig@gmail.com>
2019-07-22 19:51:28 +0200

    homework

--- staff/oihane-unciti/initial-app/main.js
+++ staff/oihane-unciti/initial-app/main.js
@@ -48,6 +48,7 @@ var registerForm = registerPanel.children[0];
 
 registerForm.addEventListener('submit', function(event) {
     event.preventDefault();
+    debugger;
 
     var name = event.target.name.value;
     var surname = event.target.surname.value;
@@ -55,19 +56,23 @@ registerForm.addEventListener('submit', function(event) {
     var password = event.target.password.value;
 
     //if name.value == "" --> Ask again
+    if (name == "" || surname == "" || email == "" || password == ""){
+        window.alert("Please enter all the information"); 
+    }else{
+        users.push({
+            name: name,
+            surname: surname,
+            email: email,
+            password: password
+        });
 
-    users.push({
-        name: name,
-        surname: surname,
-        email: email,
-        password: password
-    });
-
-    registerPanel.classList.remove('panel--show');
-    registerPanel.classList.add('panel--hide');
+        registerPanel.classList.remove('panel--show');
+        registerPanel.classList.add('panel--hide');
+        
+        registerSuccessPanel.classList.remove('panel--hide');
+        registerSuccessPanel.classList.add('panel--show');
+    }
     
-    registerSuccessPanel.classList.remove('panel--hide');
-    registerSuccessPanel.classList.add('panel--show');
 });
 
 
@@ -89,3 +94,35 @@ var registerSuccessPanel = panels[2];
 
 
 var loginPanel = panels[3];
+var aux = panels[3].children[0]
+ // es necesario este paso?
+var logButton = aux.children[4];
+var welcomePanel = panels[4];
+
+
+logButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    debugger;
+    var emailLogin = document.getElementById('email2').value; 
+    var passwordLogin = document.getElementById('password2').value; 
+    var user = {};
+    for(var i=0; i<users.length; i++){
+        if(users[i].email == emailLogin){
+            user = users[i];
+            break;
+        }
+    }
+
+    if(user &&  user.password == passwordLogin){
+        loginPanel.classList.remove('panel--show');
+        loginPanel.classList.add('panel--hide');
+
+        welcomePanel.classList.remove('panel--hide');
+        welcomePanel.classList.add('panel--show');
+
+    }else{
+        window.alert("El email y/o la contraseña son incorrectas"); 
+    }
+
+
+});
