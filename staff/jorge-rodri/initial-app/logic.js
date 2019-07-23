2f1ce299d354b20d8badf458fa75e969dfe82102
Jorge <jorgegrancanaria@gmail.com>
2019-07-23 23:40:41 +0200

    changes in logic and logic.spec

--- staff/jorge-rodri/initial-app/logic.js
+++ staff/jorge-rodri/initial-app/logic.js
@@ -1,47 +1,79 @@
 function register(name, surname, email, password){
-    var errors="";
+    var errors='';
+
     if(!name.trim()){
         errors+='Name is empty or blank.';
     }
+
     if(!surname.trim()){
         if(errors) errors+='\n';
+
         errors+='Surname is empty or blank.';
     }
+
     if(!email.trim()){
         if(errors) errors+='\n';
+
         errors+='Email is empty or blank.';
     }
+
+    if(!VALIDATE_EMAIL(email)){
+        if(errors) errors+='\n';
+
+        errors+='Email not valid.'
+    }
+
+    if(!comprobeEmail(email)){
+        if(errors) errors
+    }
+
     if(!password.trim()){
         if(errors) errors+='\n';
+
         errors+='Password is empty or blank.\n';
+    }
+
+    if(!VALIDATE_PASS(password)){
+        if(errors) errors+='\n';
+
+        errors+='Password is not valid, you need minimum 6 numerics digits.'
     }
 
     if(errors){
         throw new Error(errors);
-    }else{
+    }
+    var user= users.find(function(user){
+        return user.email === email;
+    });
+    if(user)
+        throw new Error('User already exist.')
+    else
         users.push({
             name:name,
             surname:surname,
             email:email,
             password:password
         });
-    }
 }
 
 function login(email, password){
-    var errors="";
-    if(!email.trim()){
-        errors+='Email is empty or blank';
+    var errors='';
+
+    if(!email.trim()) {
+        if (errors) errors += '\n';
+
+        errors+='E-mail is empty or blank.';
     }
+
     if(!password.trim()){
         if(errors) errors+='\n';
-        errors+='Password is empty or blank';
+        errors+='Password is empty or blank.';
     }
 
     if(errors)throw new Error(errors);
 
-    var user= ursers.find(function(){
-        return users.email==email&&users.password==password;
+    var user= users.find(function(user){
+        return user.email === email && user.password === password;
     });
     if(!user){
         throw new Error('Wrong credentials.')
@@ -49,23 +81,12 @@ function login(email, password){
 }
 
 
+function VALIDATE_PASS(pass){
+    var re=/^[0][0-9]\d{5}$|^[0-9]\d{5}$/
+    return re.test(pass);
+}
 
-function comprobeEmail(email){
-if(users.length==0){
-    return false;
-}
-for(var i=0;i<users.length;i++){
-    if(users[i].email==email){
-        return true
-    }
-}
-return false;
-}
-function validatePass(pass){
-var re=/^[0][1-9]\d{5}$|^[1-9]\d{5}$/
-return re.test(pass);
-}
-function validateEmail(email) {
-var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-return re.test(email);
+function VALIDATE_EMAIL(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
--- staff/jorge-rodri/initial-app/logic.spec.js
+++ staff/jorge-rodri/initial-app/logic.spec.js
@@ -1,29 +1,49 @@
-'use stric'
+'use strict';
 
-describe('logic', function(){
-    beforeEach(function(){
+describe('logic', function () {
+    beforeEach(function() {
         users = new Curray();
     });
 
-    describe('register', function(){
-        var user = {
-            name: 'Jorge',
-            surname: 'Rodriguez',
-            email: 'jorge@gmail.com',
-            password: '123'
-        };
+    describe('register', function () {
+        it('should succeed on correct data', function () {
+            var user = {
+                name: 'Jorge',
+                surname: 'Rodriguez',
+                email: 'jorge@gmail.com',
+                password: '123456'
+            };
 
-        register(user.name, user.surname, user.email, user.password);
+            register(user.name, user.surname, user.email, user.password);
 
-        var _user = users.pop();
+            var _user = users.pop();
 
-        expect(_user).toEqual(user);
-    });
+            expect(_user).toEqual(user);
+        });
 
-    it('should fail on empty name', function () {
-        expect(function () {
-            register('', 'Rodriguez', 'jorge@gmail.com', '123');
-        }).toThrowError(Error, 'Name is empty or blank.');
+        it('should fail on empty name', function () {
+            expect(function () {
+                register('', 'Rodriguez', 'jorge@gmail.com', '123456');
+            }).toThrowError(Error, 'Name is empty or blank.');
+        });
+
+        it('should fail to wrong email', function(){
+            expect(function(){
+                register('Jorge', 'Rodriguez', 'jorge@@gmail.com', '123456');
+            }).toThrowError(Error, 'Email not valid.')
+        })
+
+        it('should fail to wrong password', function(){
+            expect(function(){
+                register('Jorge', 'Rodriguez', 'jorge@gmail.com', '12345')
+            }).toThrowError(Error, 'Password is not valid, you need minimum 6 numerics digits.')
+        })
+
+        describe('when user already exist', function(){
+            expect(function(){
+                
+            })
+        })
     });
 
     describe('login', function () {
