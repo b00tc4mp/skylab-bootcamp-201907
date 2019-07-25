075b641465746dde421b6a15a8546beadc22e83e
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-07-25 17:39:03 +0200

    add filter.js

--- staff/jaume-villarreal/array-functions/filter.js
+++ staff/jaume-villarreal/array-functions/filter.js
@@ -1,18 +1,11 @@
-function filter(array, condition) {
+function filter(array , expression){
+    if(!arguments.length) throw TypeError('no declared arguments');
+    if(!(array instanceof Array))  throw TypeError (array + " is not an array");
+    if(!(expression instanceof Function)) throw TypeError(expression + " is not a function");
 
-    newarrays=[];
-    
-    if (condition){
-    
-        for(var i=0;i<array.length;i++){
-            
-            if (array[i].length > condition){
-                newarrays+= array[i] + ' ';
-            }
-         }
-         return  newarrays;
-        }
-     else {
-            return newarrays;
-            }
-    }    
+    var filtered = [];
+    for(var i = 0 ; i<array.length ; i++){
+        if(expression(array[i])) filtered.push(array[i]);
+    };
+    return filtered;
+};
