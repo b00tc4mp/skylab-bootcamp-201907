53bf422975215fb313c5f7d6b14753d300d385bf
albert fuente homs <example@example.com>
2019-07-27 11:31:00 +0200

    app ES6

---
+++ staff/albert-fuente/initial-app-ES6/9/data/index.js
@@ -0,0 +1,29 @@
+/**
+ * Data
+ */
+
+let users = [];
+
+users.push({
+    name: 'Manuel',
+    surname: 'Barzi',
+    email: 'manuelbarzi@gmail.com',
+    password: '123'
+})
+
+
+
+
+
+/**
+ * Data
+ */
+/* 
+let users = new Curray()
+
+users.push({
+    name: 'Manuel',
+    surname: 'Barzi',
+    email: 'manuelbarzi@gmail.com',
+    password: '123'
+}) */
