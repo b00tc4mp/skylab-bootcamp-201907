50cc427d3de840bc6d795a0ff0ec8230518ec412
Nestor Llorens <nesllorens@gmail.com>
2019-07-29 18:42:40 +0200

    on progress

--- staff/nestor-llorens/initial-app/components/index.js
+++ staff/nestor-llorens/initial-app/components/index.js
@@ -22,15 +22,15 @@ class Home extends Component {
         const enroll = this.container.getElementsByClassName('enrollButton')[0]
         enroll.addEventListener('click', function (event) {
             event.preventDefault()
-            expression()
-        })
+            expression();
+        });
     }
     onSignIn(expression) {
         const signIn = this.container.getElementsByClassName('signInButton')[0]
         signIn.addEventListener('click', function (event) {
             event.preventDefault()
             expression()
-        })
+        });
     }
 }
 
@@ -39,11 +39,37 @@ class Enroll extends Component {
         super(container)
     }
     onEnroll(expression) {
-        const enroll = this.container.getElementsByClassName('enrollButton')[0]
-        enroll.addEventListener('submit', function (event) {
+        const form = this.container.getElementsByClassName('enrollForm')[0]
+        form.addEventListener('submit', function (event) {
             event.preventDefault()
+            const name = event.target.name.value
+            const lastName = event.target.lastName.value
+            const email = event.target.email.value
+            const pass = event.target.pass.value
+            expression(name, lastName, email, pass)
+            form.reset();
+        });
+    }
+    onBack(expression) {
+        const back = this.container.getElementsByClassName('backButton')[0]
+        back.addEventListener('click', function (event) {
+            event.preventDefault();
             expression()
         })
+
+    }
+}
+
+class EnrollSuccess extends Component {
+    constructor(container) {
+        super(container)
+    }
+    onSignIn(expression) {
+        const signIn = this.container.getElementsByClassName('signInButton')[0]
+        signIn.addEventListener('click', function (event) {
+            event.preventDefault()
+            expression();
+        });
     }
 }
 
@@ -51,5 +77,58 @@ class SignIn extends Component {
     constructor(container) {
         super(container)
     }
+    onSignIn(expression) {
+        const form = this.container.getElementsByClassName('signInForm')[0]
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+
+            var email = event.target.email.value;
+            var pass = event.target.pass.value;
+            expression(email, pass)
+            form.reset();
+        })
+
+    }
+    onBack(expression) {
+        const back = this.container.getElementsByClassName('backButton')[0]
+        back.addEventListener('click', function (event) {
+            event.preventDefault();
+            expression();
+
+        })
+
+    }
 }
 
+class Landing extends Component {
+    constructor(container) {
+        super(container)
+    }
+    onLogOut(expression) {
+        const logout = this.container.getElementsByClassName('logOutButton')[0]
+        logout.addEventListener('click', function (event) {
+            event.preventDefault();
+            expression();
+
+        })
+
+    }
+}
+
+class Feedback extends Component {
+    constructor(container) {
+        super(container)
+    }
+    setMessage() {
+        this.container.innerText = message;
+    }
+
+}
+
+
+Feedback.prototype = Object.create(Component.prototype);
+Feedback.prototype.constructor = Feedback;
+
+Feedback.prototype.setMessage = function (message) {
+    this.container.innerText = message;
+};
--- staff/nestor-llorens/initial-app/index.js
+++ staff/nestor-llorens/initial-app/index.js
@@ -5,7 +5,8 @@
 const home = new Home(document.getElementsByClassName('home')[0])
 const signIn = new SignIn(document.getElementsByClassName('signIn')[0])
 const enroll = new Enroll(document.getElementsByClassName('enroll')[0])
-const enrollSuccess = new Enroll(document.getElementsByClassName('enrollSuccess')[0])
+const enrollSuccess = new EnrollSuccess(document.getElementsByClassName('enrollSuccess')[0])
+const landing = new Landing(document.getElementsByClassName('landing')[0])
 
 // home behaviour
 
@@ -21,12 +22,47 @@ home.onSignIn(function () {
 
 // enroll behaviour
 
-enroll.onEnroll(function () {
-    enroll.hide()
-    enrollSuccess.show()
+enroll.onEnroll(function (name,lastName,email,pass) {
+    try {
+        register(name, lastName, email, pass)
+        enroll.hide()
+        enrollSuccess.show()
+    }
+    catch{ console.log(error)}
+    
+})
+enroll.onBack(function () {
+        enroll.hide()
+        home.show()
 })
 
 
+// enroll success behaviour
 
+enrollSuccess.onSignIn(function () {
+    enrollSuccess.hide()
+    signIn.show()
+})
 
+// login behaviour
+
+signIn.onSignIn(function (email,password) {
+    try {
+        login(email, password)
+        signIn.hide()
+        landing.show()
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+signIn.onBack(function () {
+    signIn.hide()
+    home.show()
+})
+
+landing.onLogOut(function () {
+    landing.hide()
+    home.show()
+})
 
