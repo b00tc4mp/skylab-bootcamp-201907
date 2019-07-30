797cb63da4f1326bc3a2a0761691ef9d07715e3f
Sylvain Le Grill <sylvain@Sylvains-MacBook-Pro.local>
2019-07-30 09:31:07 +0200

    add favorite

---
+++ staff/sylvain-legrill/initial-app/components/login.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Login abstraction.
+ * 
+ * @param {HTMLElement} container 
+ */
+class Login extends SubmitBack {
+    constructor(container) {
+        super(container)
+    }
+
+    onSubmitLogin(expression) {
+        const form = this.container.children[0]
+
+        form.addEventListener('submit', event => {
+            event.preventDefault()
+
+            const email = event.target.email.value
+            const password = event.target.password.value
+
+            expression(email, password)
+        })
+    }
+}
