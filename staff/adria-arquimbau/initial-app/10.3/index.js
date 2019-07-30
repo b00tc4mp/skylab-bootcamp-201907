079c4b8b08cf8adb096521112f16a0e086bea7de
Adria Arquimbau <adria.arki@gmail.com>
2019-07-30 13:13:16 +0200

    update

---
+++ staff/adria-arquimbau/initial-app/10.3/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+/**
+ * Presentation
+ */
+
+// initial panel
+
+const landing = new Landing(document.getElementsByClassName('landing')[0]);
+
+landing.onNavigateToRegister(function () {
+    landing.hide();
+    register.show();
+});
+
+landing.onNavigateToLogin(function () {
+    landing.hide();
+    login.show();
+});
+
+// register panel
+
+const register = new Register(document.getElementsByClassName('register')[0]);
+
+register.onNavigateBack(function () {
+    register.hide();
+    landing.show();
+});
+
+register.onSubmitRegister(function (name, surname, email, password) {
+    try {
+        logic.register(name, surname, email, password);
+
+        register.hide();
+        registerSuccess.show();
+    } catch (error) {
+        register.showFeedback(error.message);
+    }
+});
+
+// register success panel
+
+const registerSuccess = new RegisterSuccess(document.getElementsByClassName('register-success')[0]);
+
+registerSuccess.onNavigateToLogin(function () {
+    registerSuccess.hide();
+    login.show();
+});
+
+// login panel
+
+const login = new Login(document.getElementsByClassName('login')[0]);
+
+login.onNavigateBack(function () {
+    login.hide();
+    landing.show();
+});
+
+login.onSubmitLogin(function (email, password) {
+    try {
+        logic.login(email, password);
+
+        login.hide();
+        home.show();
+    } catch (error) {
+        login.showFeedback(error.message);
+    }
+});
+
+// welcome panel
+
+const home = new DuckHome(document.getElementsByClassName('duck-home')[0]);
+
+home.onClickLogout(function () {
+    home.hide();
+    landing.show();
+});
+
+
+// delete Ducks.prototype.paintItem; // WHAT if...
+// home.results.paintItem = function(li, duck) { // WHAT if...
+//     console.log(duck);
+// };
+
+home.search.onSearch(function (query) {
+    logic.searchDucks(query, function(error, ducks) {
+        if (error) {
+            alert(error.message);
+        } else {
+            home.results.listItems(ducks);
+            home.results.show();
+        }
+    });
+});
+
+home.results.onClickItem = function(id) {
+    logic.retrieveDuck(id, function(error, duck) {
+        if (error) {
+            alert(error.message)
+        } else {
+            home.results.hide();
+            home.detail.displayDuck(duck);
+            home.detail.show();
+        }
+    });
+};
