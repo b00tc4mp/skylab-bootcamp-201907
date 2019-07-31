4268dc42e126fe78d4e782c4ef1406ef321cbd60
dam0ne <dam0ne@hemwick>
2019-07-31 11:10:13 +0200

    duck progress 1

---
+++ staff/jorge-garces/e-duck/1/components/initialButton.jsx
@@ -0,0 +1,27 @@
+function InitialButton({ handleVisible }) {
+  return (
+    <>
+      <a
+        href=''
+        onClick={event => {
+          event.preventDefault()
+
+          handleVisible('register')
+        }}
+      >
+        Register
+      </a>
+
+      <a
+        href=''
+        onClick={event => {
+          event.preventDefault()
+
+          handleVisible('login')
+        }}
+      >
+        Login
+      </a>
+    </>
+  )
+}
