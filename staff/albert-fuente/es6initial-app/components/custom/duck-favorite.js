2170b3bf1c2985ddd668342f78550419b1e334d5
albert fuente homs <example@example.com>
2019-07-31 12:55:41 +0200

    added files

---
+++ staff/albert-fuente/es6initial-app/components/custom/duck-favorite.js
@@ -0,0 +1,27 @@
+
+/**
+ * Favorite abstraction.
+ * 
+ * @param {HTMLElement} container 
+ */
+
+class DuckFavourite extends Component{
+    constructor(container){
+        super(container)
+    }
+    // show hide
+   /*  listItem (title) {
+        const ul = this.container.getElementsByTagName('ul')[0]
+
+        let li=document.createElement("li")
+        let h3 = document.createElement('h3')
+    
+        h3.innerText = title
+        li.appendChild(h3)
+
+        ul.appendChild(li)
+
+    } */
+
+
+}
