d3b18e78865f9040bb4de9f8b9f5c9f42ef4bc5a
albert fuente homs <example@example.com>
2019-08-02 15:02:02 +0200

    added files

---
+++ staff/albert-fuente/initial-app/components/results.js
@@ -0,0 +1,55 @@
+/**
+ * Results abstraction.
+ */
+
+ class Results extends Component{
+     constructor(container){
+         super(container)
+     }
+     listItems(items){
+        const ul = this.container.getElementsByTagName('ul')[0]
+        ul.innerHTML = ''
+    
+        items.forEach( item => {
+            const li = document.createElement('li')
+    
+            ul.appendChild(li)
+    
+            this.paintItem(li, item)
+        })/* .bind(this)) */ // WATCH this UNBIND
+    }
+    paintItem (li, item) {
+        li.innerText = item;
+    };
+
+ }
+
+
+
+
+/* 
+
+function Results(container) {
+    Component.call(this, container);
+}
+
+Results.prototype = Object.create(Component.prototype);
+Results.prototype.constructor = Results;
+
+Results.prototype.listItems = function (items) {
+    var ul = this.container.getElementsByTagName('ul')[0];
+    ul.innerHTML = '';
+
+    items.forEach(function (item) {
+        var li = document.createElement('li');
+
+        ul.appendChild(li);
+
+        this.paintItem(li, item);
+    }.bind(this)); // WATCH this
+};
+
+Results.prototype.paintItem = function(li, item) {
+    li.innerText = item;
+};
+ */
