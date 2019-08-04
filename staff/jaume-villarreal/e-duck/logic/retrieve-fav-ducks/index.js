6bc1539ee20ba28a0b4834e54e4b24d86234bfe7
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-04 14:00:22 +0200

    update retrieve-fav-ducks

--- staff/jaume-villarreal/e-duck/logic/retrieve-fav-ducks/index.js
+++ staff/jaume-villarreal/e-duck/logic/retrieve-fav-ducks/index.js
@@ -1,22 +1,20 @@
 logic.retrieveFavDucks = function(id, token) {
     validate.string(id, 'id')
     validate.string(token, 'token')
-    const ducks = []
-    // validate.function(expression, 'expression')
-debugger
+
     return call(`https://skylabcoders.herokuapp.com/api/user/${id}`, 'get', { 'authorization': `bearer ${token}` } , undefined )
         .then(response => {
             if (response.status === 'KO') throw new Error (response.error)
-            
-            else {
 
+            else {
                 const favorites = response.data.favorites
 
-                if (!favorites.length)  ducks=[]
+                if (!favorites.length)  return []
 
                 else{
+                    const ducks = []
                     favorites.forEach( duckId => {
-                        return call('http://duckling-api.herokuapp.com/api/ducks/' + duckId, undefined, undefined, undefined)
+                        call('http://duckling-api.herokuapp.com/api/ducks/' + duckId, undefined, undefined, undefined)
                             .then(duck => {
                                         if (response.status === 'KO'){ throw new Error (response.error)
                                         } else {
@@ -24,11 +22,11 @@ debugger
                                             ducks.push(duck)
                                             console.log(ducks)
                                         }
-                                    return ducks
                                 })
                             })
+                            return ducks
                         }
                     }
-            return ducks 
            })
+           catch(error => error)
         }
