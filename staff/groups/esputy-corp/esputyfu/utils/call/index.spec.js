c602c21664cbf41a5dc5a075f695d1db353d62c9
Nestor <nestor@pop-os.localdomain>
2019-08-05 10:56:31 +0200

    added token retrieve test

--- staff/groups/esputy-corp/esputyfu/utils/call/index.spec.js
+++ staff/groups/esputy-corp/esputyfu/utils/call/index.spec.js
@@ -1,10 +1,27 @@
 describe('call', () => {
+
+    // xdescribe('api spotify get token', () => { // NOT POSSIBLE TO GET TOKEN FROM BROWSER FOR SECURITY REASONS AS CLIENT CREDENTIALS ARE SENT IN PLAIN TEXT
+
+    //     it('should succeed on correct data', done => {
+    //         return call(`https://accounts.spotify.com/api/token`,
+    //         'post', 
+    //         { 'Authorization': `Basic NTQ3N2NhOWQ5NmY2NDZhMmI4NjQ0M2M0MDBmY2FlZDA6OTA4YmI2MGJlMTIyNGJkOTkzZmZjZDY2NWVmNDU5ZDk=`,
+    //         'Content-Type':'application/x-www-form-urlencoded' }, 
+    //         { "grant_type": "client_credentials" } )
+
+    //         .then(res => {
+    //             expect(res).toBeDefined()
+    //             done()
+    //         })
+    //         .catch(error => expect(error).toBeUndefined())
+
+    //     })
+    // })
     
-    describe('api spotify', () => {
-        const token = "BQDprgWYDeLRDbylEF46HRaOR6zU4n2uMhb-Rrsa8u_Q0-Xmby_S9KOBjmhfKHwxlNAi8QcJQ8AE8kpkXS8"
+    describe('api spotify get tracks', () => {
+        const token = "BQBe_1I2EcsHumCDavWtWXHjcMBHrvS6w81ajt8w_W0n3pV2SEgQ0mfhDEtoYaS10-Nz5VTa91Q6mFrYHvA"
         const query = 'wonderwall'
         const types = 'track'
-        // const num = '30'
 
         it('should succeed on correct data', done => {
             return call(`https://api.spotify.com/v1/search?q=${query}&type=${types}`,
@@ -18,14 +35,8 @@ describe('call', () => {
                 done()
             })
             .catch(error => expect(error).toBeUndefined())
-
         })
 
-
-
-
     })
-
-
 
 })
