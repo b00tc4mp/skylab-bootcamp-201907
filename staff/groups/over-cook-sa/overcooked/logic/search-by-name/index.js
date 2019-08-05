03c697589d87554e2bf939adab33fe210f66d360
dam0ne <dam0ne@hemwick>
2019-08-05 18:00:30 +0200

    updated search-by-name to use favorites

--- staff/groups/over-cook-sa/overcooked/logic/search-by-name/index.js
+++ staff/groups/over-cook-sa/overcooked/logic/search-by-name/index.js
@@ -1,9 +1,29 @@
-logic.searchByName = function (name) {
-    validate.string(name, 'name')
+logic.searchByName = function (id, token, name) {
 
-    return call(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`, 'get', undefined, undefined)
-        .then(response => {
-            if (response.meals === null) throw new Error('No meals found')
-            return response.meals
-        })
+    let favorites
+
+    if (id != undefined && token != undefined) {
+        validate.string(id, 'id')
+        validate.string(token, 'token')
+        validate.string(name, 'name')
+
+        return call(`https://skylabcoders.herokuapp.com/api/user/${id}`, 'get', {
+                'authorization': `bearer ${token}`
+            }, undefined)
+            .then(response => {
+                if (response.status === 'KO') throw new Error(response.error)
+
+                favorites = response.data.favorites
+
+                return call(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`, 'get', undefined, undefined)
+                    .then(response => {
+                        if (response.meals === null) throw new Error('No meals found')
+                        else {
+                            favorites && response.meals.forEach(meal => meal.favorite = favorites.includes(meal.idMeal))
+
+                            return response.meals
+                        }
+                    })
+            })
+    }
 }
