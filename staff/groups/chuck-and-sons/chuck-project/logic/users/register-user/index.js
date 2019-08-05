b36d8a2a820c23af9e239a4c2c32be8a89f3ae7a
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-05 16:02:30 +0200

    update logic and testing for user register

--- staff/groups/chuck-and-sons/chuck-project/logic/users/register-user/index.js
+++ staff/groups/chuck-and-sons/chuck-project/logic/users/register-user/index.js
@@ -1,4 +1,23 @@
+logic.registerUser = function(name , surname , username , password, repassword) {
+    validate.string(name , 'name')
+    validate.string(surname , 'surname')
+    validate.string(username , 'username')
+    validate.email(username , 'username')
+    validate.string(password , 'password')
 
-logic.registerUser(name , surname , username , password , repassword){
-    validate.
+    if(password !== repassword) throw Error ("password doesn't match")
+
+    const user = {
+        "name" : name,
+        "surname" : surname,
+        "username" : username,
+        "password" : password,
+        "project" : "chuck",
+        "favorites" : []
+    }
+
+    call("https://skylabcoders.herokuapp.com/api/user" , 'post' , {'content-type' : 'application/json'} , user)
+    .then(response => {
+        if (response.status === 'KO') throw Error (response.error)
+    })
 }
