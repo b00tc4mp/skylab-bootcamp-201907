3d9fa8481d618227873a02de55a85a7540a023a3
Nestor <nestor@pop-os.localdomain>
2019-08-06 17:19:17 +0200

    refactor call, added token generator test

--- staff/groups/esputy-corp/esputyfu/utils/call/index.spec.js
+++ staff/groups/esputy-corp/esputyfu/utils/call/index.spec.js
@@ -1,86 +1,91 @@
 describe('call', () => {
 
-    // xdescribe('api spotify get token', () => { // NOT POSSIBLE TO GET TOKEN FROM BROWSER FOR SECURITY REASONS AS CLIENT CREDENTIALS ARE SENT IN PLAIN TEXT
+    describe('api spotify get token', () => {
 
-    //     it('should succeed on correct data', done => {
-    //         return call(`https://accounts.spotify.com/api/token`,
-    //         'post', 
-    //         { 'Authorization': `Basic NTQ3N2NhOWQ5NmY2NDZhMmI4NjQ0M2M0MDBmY2FlZDA6OTA4YmI2MGJlMTIyNGJkOTkzZmZjZDY2NWVmNDU5ZDk=`,
-    //         'Content-Type':'application/x-www-form-urlencoded' }, 
-    //         { "grant_type": "client_credentials" } )
+        it('should succeed on correct data', () =>
+            call(`http://skylabcoders.herokuapp.com/proxy?url=https://accounts.spotify.com/api/token`,
+                'post',
+                {
+                    'Authorization': `Basic NTQ3N2NhOWQ5NmY2NDZhMmI4NjQ0M2M0MDBmY2FlZDA6OTA4YmI2MGJlMTIyNGJkOTkzZmZjZDY2NWVmNDU5ZDk=`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                'grant_type=client_credentials')
+                .then(res => {
+                    expect(res).toBeDefined()
+                })
 
-    //         .then(res => {
-    //             expect(res).toBeDefined()
-    //             done()
-    //         })
-    //         .catch(error => expect(error).toBeUndefined())
+        )
+    })
 
-    //     })
-    // })
-    
-    describe('spotify api get tracks', () => {
-
-        const query = 'wonderwall'
-        const types = 'track'
-        const api = 'spotify'
-
-        it('should succeed on correct data', () => 
-            call(`https://api.spotify.com/v1/search?q=${query}&type=${types}`,
-            'get', 
-            { 'Authorization': `Bearer ${token}` }, 
-            undefined,
-            api)
-
-            .then(res => {
-                expect(res).toBeDefined()
-                expect(res.tracks.items.length).toBe(20)
-            })
-            .catch(error => expect(error).toBeUndefined())
+    fdescribe('spotify api get tracks', () => {
+        
+        let _token
+        
+        beforeEach(() =>
+            //get new token
+            fetch(`http://skylabcoders.herokuapp.com/proxy?url=https://accounts.spotify.com/api/token`,
+                {method: 'post',
+                 headers: {'Authorization': `Basic NTQ3N2NhOWQ5NmY2NDZhMmI4NjQ0M2M0MDBmY2FlZDA6OTA4YmI2MGJlMTIyNGJkOTkzZmZjZDY2NWVmNDU5ZDk=`,
+                'Content-Type': 'application/x-www-form-urlencoded'},
+                 body:'grant_type=client_credentials'}
+            ).then(res => res.json())
+             .then(res => _token = res.access_token)
         )
 
-        it('should throw error on wrong method (spotify api only accepts "get")', () => 
-            expect(() => 
-                call(`https://api.spotify.com/v1/search?q=${query}&type=${types}`,
-                'post', 
-                { 'Authorization': `Bearer ${token}` }, 
+        it('should succeed on correct data', () =>
+            call(`https://api.spotify.com/v1/search?q=${query}&type=${types}`,
+                'get',
+                { 'Authorization': `Bearer ${_token}` },
                 undefined,
-                api)).toThrowError(Error, 'method with value post does not match one of the expected values: get'))
-            
+                )
+
+                .then(res => {
+                    expect(res).toBeDefined()
+                    expect(res.tracks.items.length).toBe(20)
+                })
+
+        )
+
     })
 
     describe('skylab api methods tests', () => {
-        const api = 'skylab'
-        
-        it('post should succeed on correct data', () => {
-            return call('https://skylabcoders.herokuapp.com/api/user', 'post', { 'content-type': 'application/json' }, {
+
+        let body
+
+        let _body
+        beforeEach(() =>
+
+            body = {
                 username: `esputy-${Math.random()}`,
                 password: `esputy-${Math.random()}`
-            },
-            api)
-                .then(res => expect(res).toBeDefined())
-                .catch(error => expect(error).toBeUndefined())
-        })
-        it('get should succeed on correct url and method', () => {
-            return call('https://skylabcoders.herokuapp.com/api/users', 'get', undefined, undefined, api) 
-                
-                .then(res => expect(res).toBeDefined())
-                .catch(error => expect(error).toBeUndefined())
-        })
+            }
+        )
 
-        //TO DO PATCH, PUT, DELETE
-    })
-    describe('api tests', () => {
-        const api = 'random'
+        _body = JSON.stringify(body)
+
         
-        it('"should throw error when api is not "spotify" or "skylab"', () => {
-            expect(() => 
-                call(`https://api.spotify.com/v1/search?q=wonderwall&type=track`,
-                'post', 
-                { 'Authorization': `Bearer ${token}` }, 
+        it('post should succeed on correct data', () => {
+            return call('https://skylabcoders.herokuapp.com/api/user',
+                'post',
+                { 'content-type': 'application/json' },
+                _body)
+                    .then(res => expect(res).toBeDefined())
+
+            }
+        )
+
+        it('get should succeed on correct url and method', () => {
+            return call('https://skylabcoders.herokuapp.com/api/users',
+                'get',
                 undefined,
-                api)).toThrowError(Error, 'random must be "spotify or "skylab"')
-        })
+                undefined)
+
+                .then(res => expect(res).toBeDefined())
+
     })
+
+    //TO DO PATCH, PUT, DELETE
+})
 
 })
 
