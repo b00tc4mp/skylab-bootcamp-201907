2231c1fea7526e1c7607501056444c12d5104810
Adria Arquimbau <adria.arki@gmail.com>
2019-08-06 12:23:55 +0200

    up comp landing

--- staff/groups/over-cook-sa/overcooked/components/landing.jsx
+++ staff/groups/over-cook-sa/overcooked/components/landing.jsx
@@ -1,62 +1,21 @@
-const { component } = React
+const { Component } = React
 
 class Landing extends Component {
-    constructor() {
+    constructor () {
         super()
 
-        this.state = { view: 'X', user: undefined }
+        this.state = { view: 'header'}
 
-        this.handleRegister = this.handleRegister.bind(this)
-        this.handleLogin = this.handleLogin.bind(this)
-        this.handleRandomRecipe = this.handleRandomRecipe.bind(this)
-        this.handleRegisterSucces = this.handleRegisterSucces.bind(this)
     }
 
-    // handleRegister(event) {
-    //     event.preventDefault()
+    render() {
 
-    //     this.props.onRegister()
-    // }
+        const { state: { view } } = this
 
-    // handleLogin(event) {
-    //     event.preventDefault()
-
-    //     this.props.onLogin()
-    // }
-
-    // handleRandomRecipe(event) {
-    //     event.preventDefault()
-
-        
-    // }
-
-    // handleRegisterSucces(event) {
-    //     event.preventDefault()
-
-    // }
-
-    render(){
         return <>
-            <header>
-                <img src="" alt=""/>
-                <h1>OVERCOOKED</h1>
+            
 
-            </header>
-            <main>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="" onClick="">Register</a>
-                        </li>
-                        <li>
-                            <a href="" onClick="">Login</a>
-                        </li>
-                    </ul>
-                </nav>
-                <section>
-                    <RandomRecipe />
-                </section>
-            </main> 
         </>
     }
+
 }
