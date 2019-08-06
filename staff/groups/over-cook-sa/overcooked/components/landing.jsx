efd9701037b08de913b99aa8297a6ca0fb88b374
dam0ne <dam0ne@hemwick>
2019-08-06 18:37:10 +0200

    updated register success

--- staff/groups/over-cook-sa/overcooked/components/landing.jsx
+++ staff/groups/over-cook-sa/overcooked/components/landing.jsx
@@ -1,75 +1,80 @@
 const { Component } = React
 
 class Landing extends Component {
-    constructor () {
-        super()
+  constructor() {
+    super()
 
+    this.state = { view: 'first', mealRandom: null }
 
-        this.state = { view: 'first', mealRandom: null }
-        
-        this.onRandomRecipe = this.onRandomRecipe.bind(this)
-        this.handleGoToLogin = this.handleGoToLogin.bind(this)
-        this.handleGoToRegister = this.handleGoToRegister.bind(this)
-        this.handleRegister = this.handleRegister.bind(this)
+    this.onRandomRecipe = this.onRandomRecipe.bind(this)
+    this.handleGoToLogin = this.handleGoToLogin.bind(this)
+    this.handleGoToRegister = this.handleGoToRegister.bind(this)
+    this.handleRegister = this.handleRegister.bind(this)
+  }
 
+  componentWillMount() {
+    this.onRandomRecipe()
+  }
 
+  onRandomRecipe = () => {
+    logic.retrieveRandomRecipe().then(meal => {
+      //console.log(meal)
+      this.setState({ mealRandom: meal })
+    })
+  }
+
+  handleRegister(name, surname, email, password, repassword) {
+    try {
+      logic
+        .registerUser(name, surname, email, password, repassword)
+        .then(() => this.setState({ view: 'register-success' }))
+        .catch(({ message }) => this.setState({ error: message }))
+    } catch ({ message }) {
+      this.setState({ error: message })
     }
+  }
 
-    componentWillMount(){
-        this.onRandomRecipe()
-    }
+  handleGoToRegister() {
+    this.setState({ view: 'register', mealRandom: null })
+  }
 
-    onRandomRecipe = () => {
-        logic.retrieveRandomRecipe()
-        .then(meal => {
-            //console.log(meal)
-            this.setState({mealRandom : meal})
-        })
-    }
+  handleGoToLogin() {
+    this.setState({ view: 'login', mealRandom: null })
+  }
 
-    handleRegister(name, surname, email, password, repassword) {
-        try {
-            logic.registerUser(name, surname, email, password, repassword)
-                .then(() => this.setState({ view: 'register-success' }))
-                .catch(({ message }) => this.setState({ error: message }))
-        } catch ({ message }) {
-            this.setState({ error: message })
-        }
-    }
+  render() {
+    const {
+      state: { view, mealRandom },
+      handleGoToLogin,
+      handleGoToRegister,
+      handleRegister
+    } = this
 
-    handleGoToRegister() {
-        this.setState({ view: 'register', mealRandom: null })
-    }
-
-    handleGoToLogin() {
-        this.setState({ view: 'login' , mealRandom: null })
-
-    }
-    
-
-    render() {
-
-        const { state: { view, mealRandom }, handleGoToLogin, handleGoToRegister, handleRegister } = this
-
-        return <>
-            <header>
-                <BigHeader />
-            </header>
-            <main>
-                <section>
-                    {view === "register" && <Register onRegister={handleRegister} />}
-                    { view === 'first' && <WelcomeAnchors onLogin={handleGoToLogin} onRegister={handleGoToRegister} />}
-                    { view === 'login' && <Login />}
-                </section>
-                <section>
-                    {mealRandom && <RecipeItem meal={mealRandom} />}
-                </section>
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-
-        </>
-    }
-
+    return (
+      <>
+        <header>
+          <BigHeader />
+        </header>
+        <main>
+          <section>
+            {view === 'register' && <Register onRegister={handleRegister} />}
+            {view === 'first' && (
+              <WelcomeAnchors
+                onLogin={handleGoToLogin}
+                onRegister={handleGoToRegister}
+              />
+            )}
+            {view === 'login' && <Login />}
+            {view === 'register-success' && (
+              <RegisterSuccess onLogin={handleGoToLogin} />
+            )}
+          </section>
+          <section>{mealRandom && <RecipeItem meal={mealRandom} />}</section>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </>
+    )
+  }
 }
