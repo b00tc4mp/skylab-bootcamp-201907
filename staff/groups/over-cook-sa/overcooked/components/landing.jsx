b3666c91659df904dfdc8b9c0dc41f42c7a0a4ff
Adria Arquimbau <adria.arki@gmail.com>
2019-08-06 11:06:32 +0200

    update landing page

--- staff/groups/over-cook-sa/overcooked/components/landing.jsx
+++ staff/groups/over-cook-sa/overcooked/components/landing.jsx
@@ -4,40 +4,59 @@ class Landing extends Component {
     constructor() {
         super()
 
-        this.state = { view: 'search', query: undefined,/*ducks: [], duck: undefined,*/ error: undefined, user: undefined, favs: [] }
+        this.state = { view: 'X', user: undefined }
 
         this.handleRegister = this.handleRegister.bind(this)
         this.handleLogin = this.handleLogin.bind(this)
         this.handleRandomRecipe = this.handleRandomRecipe.bind(this)
+        this.handleRegisterSucces = this.handleRegisterSucces.bind(this)
     }
 
-    handleRegister(event) {
-        event.preventDefault()
+    // handleRegister(event) {
+    //     event.preventDefault()
 
-        this.props.onRegister()
-    }
+    //     this.props.onRegister()
+    // }
 
-    handleLogin(event) {
-        event.preventDefault()
+    // handleLogin(event) {
+    //     event.preventDefault()
 
-        this.props.onLogin()
-    }
+    //     this.props.onLogin()
+    // }
 
-    handleRandomRecipe(event) {
-        event.preventDefault()
+    // handleRandomRecipe(event) {
+    //     event.preventDefault()
 
         
-    }
+    // }
 
+    // handleRegisterSucces(event) {
+    //     event.preventDefault()
 
-
-
-
-
+    // }
 
     render(){
         return <>
-            
+            <header>
+                <img src="" alt=""/>
+                <h1>OVERCOOKED</h1>
+
+            </header>
+            <main>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="" onClick="">Register</a>
+                        </li>
+                        <li>
+                            <a href="" onClick="">Login</a>
+                        </li>
+                    </ul>
+                </nav>
+                <section>
+                    <RandomRecipe />
+                </section>
+            </main> 
         </>
     }
 }
