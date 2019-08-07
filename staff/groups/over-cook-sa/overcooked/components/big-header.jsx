cdd50e790e32d0ac9a338822e417823d3f3c41f2
Adria Arquimbau <adria.arki@gmail.com>
2019-08-07 17:13:50 +0200

    done debuggin styles

--- staff/groups/over-cook-sa/overcooked/components/big-header.jsx
+++ staff/groups/over-cook-sa/overcooked/components/big-header.jsx
@@ -3,5 +3,5 @@ function BigHeader () {
     return <>
         <img className = "landing-logo" src="https://cdn.shopify.com/s/files/1/0014/1962/files/FG-banner_collect-top_template2_overcooked.png?v=1528818873" alt=""/>
     </>
-  )
+  
 }
