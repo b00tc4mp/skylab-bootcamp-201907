48154c095f527913892e57800481c913f378b511
Felipe <felipefcor@gmail.com>
2019-08-07 16:53:15 +0200

    change grid panel in results

--- staff/groups/alf-&-co/app/components/results/index.jsx
+++ staff/groups/alf-&-co/app/components/results/index.jsx
@@ -1,12 +1,14 @@
 function Results(props) {
-    return <ul>
+    return <div className="mosaicPanel">
+        <ul className="mosaicPanel">
         {/* For each movie in movies array, create a list element and add event listener with closure to it*/}
-        {props.movies.map(movie => <li key={movie.id} onClick={ () => {
+        {props.movies.map(movie => <li className="mosaicList" key={movie.id} onClick={ () => {
             
             props.onItem(movie.id.toString())
         }}>
         {/* Send individual movie to paintItem which will create its visualization in the grid */}
         {props.paintItem(movie)}
         </li>)}
-    </ul>
+        </ul>
+    </div>
 }
