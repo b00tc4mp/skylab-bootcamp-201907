b247982f173a33fcbab63f4c7f5bf7a94c263444
Nestor <nestor@pop-os.localdomain>
2019-08-07 11:59:06 +0200

    register & register-success first commit

--- staff/groups/esputy-corp/esputyfu/components/app.jsx
+++ staff/groups/esputy-corp/esputyfu/components/app.jsx
@@ -2,17 +2,41 @@ class App extends React.Component {
     constructor() {
         super()
 
-        this.state = { view: 'undefined' }
-
+        this.state = { view: 'register' }
         
+        //bindings
+
+        this.handleRegister = this.handleRegister.bind(this)
+        this.handleBackToLanding = this.handleBackToLanding.bind(this)
+    }
+    //register
+
+    handleRegister(name, surname, email, password, repassword) {
+        try {
+            logic.registerUser(name, surname, email, password, repassword)
+            .catch(console.error)
+
+            this.setState({ view: 'register-success' })
+        } catch (error) {
+            console.error(error)
+        }
     }
 
+    handleBackToLanding() {
+        this.setState({ view: 'landing' })
+    }
+    //register-success
+
+    handleGoToLogin() {
+        this.setState({ view: 'login'})
+    }
 
 
     render() {
 
         return <>
-        
+            {this.state.view === 'register' && <Register onRegister={this.handleRegister}/>}
+            {this.state.view === 'register-success' && <RegisterSuccess onLogin={this.handleGoToLogin}/>}
         </>
     }
 }
---
+++ staff/groups/esputy-corp/esputyfu/components/register-success.jsx
@@ -0,0 +1,8 @@
+function RegisterSuccess(props) {
+    return <p>
+        Gracias por registrarse, puede proceder a<a href="" onClick={event => {
+            event.preventDefault()
+            props.onLogin()
+        }}>Iniciar Sesión</a>.
+    </p>
+}
