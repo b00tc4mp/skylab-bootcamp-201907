a86b474d0e120787e4274d6887021b94471639cd
Adria Arquimbau <adria.arki@gmail.com>
2019-08-07 20:16:13 +0200

    up

--- staff/groups/over-cook-sa/overcooked/components/results.jsx
+++ staff/groups/over-cook-sa/overcooked/components/results.jsx
@@ -1,7 +1,9 @@
 function Results(props) {
    return <ul>
         {props.meals.map( meal => 
-            <li key={meal.idMeal} >
+            <li key={meal.idMeal} onClick={ () => {
+                props.onMeal(meal.idMeal)
+            }} >
                 {props.paintMeal(meal)}
             </li>)
         }
