98d0074ecb51f3966b6a331ea7ae23d52413fa24
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-08-07 13:28:30 +0200

    retrieve lyrics logic and happy path tests ready

---
+++ staff/groups/abesti-enterprise/bertsolab/logic/retrieve-lyrics/index.js
@@ -0,0 +1,43 @@
+logic.retrieveLyrics = function (id, token, track_id){
+    //let favorites
+
+    if(id != undefined && token != undefined) {
+        validate.string(id, 'id')
+        validate.string(token, 'token')
+        validate.string(track_id, 'track_id')
+
+        return call(`https://skylabcoders.herokuapp.com/api/user/${id}`, 'get', { 'authorization': `bearer ${token}` }, undefined)
+        .then(res => {
+            if(res.status === 'KO') throw new Error (response.error)
+
+            favorites = response.data.favorites
+
+            return call(`http://skylabcoders.herokuapp.com/proxy?url=https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=e492562d27469098b0922d5d580837eb&track_id=${track_id}`, 'get', undefined, undefined)
+            .then(res => {
+                if(res.message.header.status_code === 404) throw new Error("invalid token")
+                else {
+                    const lyrics = res.body.lyrics.lyrics_body
+
+                    //favorites && track_id.favorite.includes() --> Verlo con Oihane
+                    //favorites && (duck.favorite = favorites.includes(duckId))
+
+                    return lyrics
+                }
+            })
+        })
+    } else {
+        validate.string(track_id, 'track_id')
+        // debugger
+
+        return call(`http://skylabcoders.herokuapp.com/proxy?url=https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=e492562d27469098b0922d5d580837eb&track_id=${track_id}`, 'get', undefined, undefined)
+        .then(res => {
+            console.log(res)
+            if(res.message.header.status_code === 401) throw new Error("invalid token")
+            else {
+                const lyrics = res.message.body.lyrics.lyrics_body
+
+                return lyrics
+            }
+        })
+    }
+}
--- staff/groups/abesti-enterprise/bertsolab/logic/search-songs/index.js
+++ staff/groups/abesti-enterprise/bertsolab/logic/search-songs/index.js
@@ -30,7 +30,7 @@ logic.searchSongs = function (id, token, q_artist, q_track) {
 
         return call(`http://skylabcoders.herokuapp.com/proxy?url=https://api.musixmatch.com/ws/1.1/track.search?apikey=e492562d27469098b0922d5d580837eb&q_artist=${q_artist}&q_track=${q_track}`, 'get', undefined, undefined)
             .then(res => {
-                if(res.message.header.status_code === 401) throw new Error("invalid token")
+                if(res.message.header.status_code === 401) throw new Error("invalid artist or song")
                 
                 const { message: { body: { track_list }}} = res
                 return track_list       
