177931a9ad4c0ef04939b2bd4f7c724eec447309
xossse <xossse@MacBook-Pro-de-xossse.local>
2019-08-07 16:32:14 +0200

    add search

--- staff/groups/esputy-corp/esputyfu/components/landing.jsx
+++ staff/groups/esputy-corp/esputyfu/components/landing.jsx
@@ -1,12 +1,63 @@
 class Landing extends React.Component {
-    constructor () {
+    constructor() {
         super()
+
+        this.state = { search: undefined, tracks: [] }
+
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleLogin = this.handleLogin.bind(this)
+        this.handleRegister = this.handleRegister.bind(this)
+
+    }
+
+    handleSearch(query = this.state.query) {
+        try {
+            logic.searchTracks( this.props.data.id, this.props.data.token, query)
+                .catch(message => console.error(message))
+                .then(response => {
+                    this.setState({ search: query })
+                    this.setState({ tracks: response })
+                    console.log(response)
+                })
+        } catch {
+            console.error(error)
+        }
+    }
+
+    handleLogin(event) {
+        event.preventDefault()
+        this.props.onLogin()
+    }
+
+    handleRegister(event) {
+        event.preventDefault()
+        this.props.onRegister()
     }
 
 
     render() {
         return <>
-        Hola
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#" >Favoritos</a></li>
+                        <li><a href="" onClick={this.handleLogin}>Iniciar sesión</a></li>
+                        <li><a href="" onClick={this.handleRegister}>Registro</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <section>
+                    <Search onSearch={this.handleSearch} />
+                </section>
+                <section>
+
+                </section>
+
+            </main>
+            <footer>
+                <p>Hemos utilizado la api de Spotify. Que quede claro.</p>
+            </footer>
         </>
-    } 
+    }
 }
