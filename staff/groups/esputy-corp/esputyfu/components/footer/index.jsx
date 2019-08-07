18fdf76e1a87bec96370600ec86cae9556ed0e37
Roger Perez Serrano <Roger@MacBook-Air-de-Roger.local>
2019-08-08 01:05:06 +0200

    add footer style

--- staff/groups/esputy-corp/esputyfu/components/footer/index.jsx
+++ staff/groups/esputy-corp/esputyfu/components/footer/index.jsx
@@ -1,7 +1,11 @@
 function Footer() {
     return <>
-        <footer>
-                <p>Hemos utilizado la api de Spotify. Que quede claro.</p>
+        <footer class='footer'>
+                <div class='footer__container'>
+                    <h2 class='footer__title'>Esputy Corp®</h2>
+                    <p class='footer__p'>Hemos utilizado la api de Spotify. Que quede claro.</p>
+                </div>
+                
             </footer>
     </>
 
