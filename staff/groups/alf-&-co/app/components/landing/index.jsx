6862a5b810b1f44c1ba8de67a7097c780af2bc1e
aramanjon <aracelimanjoncb@gmail.com>
2019-08-08 23:33:21 +0200

    detail layout

--- staff/groups/alf-&-co/app/components/landing/index.jsx
+++ staff/groups/alf-&-co/app/components/landing/index.jsx
@@ -5,7 +5,7 @@ class Landing extends Component {
         super()
 
 
-        this.state = { view: 'collections', search: false, query: undefined, movieId: undefined, collection: undefined, movies: [], movie: undefined, error: undefined, user: undefined, favs: [], lists: undefined}
+        this.state = { view: 'collections', search: true, query: undefined, movieId: undefined, collection: undefined, movies: [], movie: undefined, error: undefined, user: undefined, favs: [], lists: undefined}
 
         this.handleGoToFavorites = this.handleGoToFavorites.bind(this)
         this.handleGoToCollections = this.handleGoToCollections.bind(this)
@@ -302,7 +302,7 @@ handleGoToHome(event){
                 </nav>
             </header>
             <main>
-                {/* Search state is false by default. It's only displayed when clicked on search button */}
+                {/* Search state is true by default. */}
                 {search && <Search onSearch={handleSearch}></Search>}
 
                 {/* Default view on landing <main>. Displays collections of movies. */}
--- staff/groups/alf-&-co/app/components/movie-detail/index.jsx
+++ staff/groups/alf-&-co/app/components/movie-detail/index.jsx
@@ -3,24 +3,37 @@ function MovieDetail({movie, onBack, onToggle}){
     return <>
     {/* Only displayed after query search or click on a collection. 
     Composed by a grid of movie items with title, rating, poster and a fav button */}
-        <img src={`http://image.tmdb.org/t/p/w342/${movie.movieComplete.poster_path}`} />
-        <FavButton active={movie.movieComplete.favorite} onToggle={() => onToggle(movie.movieComplete.id.toString())} />
-        <h3>{ movie.movieComplete.original_title }</h3>
-        <span>{ movie.movieComplete.release_date }</span>
-        <h4>{ movie.movieComplete.tagline }</h4>
-        <span>{movie.movieComplete.vote_average}</span>
-        
-        <h4>Director</h4>
-        <p>{movie.director}</p>
-        <h4>Overview</h4>
-        <span>{ movie.movieComplete.overview }</span>
-        <h4>Main Cast</h4>
-        {movie.mainCast.map(movie => <li key={movie}>{movie}</li>)}
+        <div className="containerDetail">
 
-        {onBack && <a href="" onClick={ event => {
+        <div className= "panelDetail">
+        <h3 className="panelDetail__title">{ movie.movieComplete.original_title }</h3>
+        <span className="panelDetail__vote">{movie.movieComplete.vote_average}</span>
+        <FavButton active={movie.movieComplete.favorite} onToggle={() => onToggle(movie.movieComplete.id.toString())} />
+        {/* <h4 className="panelDetail__tagline">{ movie.movieComplete.tagline }</h4> */}
+        
+        
+        <span className="panelDetail__date">{ movie.movieComplete.release_date }</span>
+        
+        
+        <h4 className="panelDetail__director">Director</h4>
+        <p className="panelDetail__director--name">{movie.director}</p>
+        <h4 className="panelDetail__mainCast">Main Cast</h4>
+        {movie.mainCast.map(movie => <li className="panelDetail__mainCast--name"key={movie}>{movie}</li>)}
+        
+        
+        <h4 className="panelDetail__description">Overview</h4>
+        <span className="panelDetail__description--description">{ movie.movieComplete.overview }</span>
+        </div>
+        <div className= "panelImage">
+        <img className="panelImage__image"src={`http://image.tmdb.org/t/p/w342/${movie.movieComplete.poster_path}`} />
+        </div>
+        {onBack && <a className="close__detail" href="" onClick={ event => {
             event.preventDefault()
 
             onBack()
-        }}>Go back</a>}
+        }}></a>}
+        </div>
+        
+        
     </>
 }
