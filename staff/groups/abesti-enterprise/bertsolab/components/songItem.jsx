044730c98a5f96e71a2a497340a1f6c5a893b80d
Oihane <oihaneuncitig@gmail.com>
2019-08-08 16:11:38 +0200

    update prueba fav

--- staff/groups/abesti-enterprise/bertsolab/components/songItem.jsx
+++ staff/groups/abesti-enterprise/bertsolab/components/songItem.jsx
@@ -1,10 +1,10 @@
-function SongItem({ track: { track_id, track_name, artist_name, album_name}, onDisplay }) {
+function SongItem({ track: { track_id, track_name, artist_name, album_name, favorite}, onDisplay, onToggle}) {
     return <>
         <h3>{track_name}</h3>
         <h4>{artist_name}</h4>
         <h5>{album_name}</h5>
 
-        {/* { <FavButton active={favorite} onToggle={() => onToggle(commontrack_id)} /> }  */}
+        { <FavButton active={favorite} onToggle={() => onToggle(track_id)} /> } 
         { <DisplayLyrics onDisplay={() => onDisplay(track_id)} />  }
 
     </>
