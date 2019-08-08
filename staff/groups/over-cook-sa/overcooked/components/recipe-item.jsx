ab6eddc555fd8869cbd18239569ff8bbd8f08240
Adria Arquimbau <adria.arki@gmail.com>
2019-08-08 23:32:08 +0200

    úp

--- staff/groups/over-cook-sa/overcooked/components/recipe-item.jsx
+++ staff/groups/over-cook-sa/overcooked/components/recipe-item.jsx
@@ -3,7 +3,7 @@ function RecipeItem ( { meal: { strMeal, strYoutube } } ) {
 
     <section className="random-content">
         <h2 className="random-recipe">Random Recipe</h2>        
-        <iframe className="random-recipe recipe-video" width="420" height="315" src={strYoutube}></iframe>   
+        <iframe className="random-recipe recipe-video" src={strYoutube}></iframe>   
         <h3 className="random-recipe recipe-title">{strMeal}</h3>
     </section> 
     </>
