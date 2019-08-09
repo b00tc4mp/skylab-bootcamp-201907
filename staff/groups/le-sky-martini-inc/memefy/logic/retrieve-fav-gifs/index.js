d4497aea4e2f92a6b13b27de43b7f5fbd1c13780
Lluis <lluisagustisan@gmail.com>
2019-08-09 13:54:26 +0200

    update documentation logic 1

--- staff/groups/le-sky-martini-inc/memefy/logic/retrieve-fav-gifs/index.js
+++ staff/groups/le-sky-martini-inc/memefy/logic/retrieve-fav-gifs/index.js
@@ -1,3 +1,18 @@
+/**
+ * It allows a registered and logged user to check or remove favorite GIFs from its favorites array.
+ * Check: renders all user's favorite gifs by id.
+ * Remove: It checks whether the GIF id already exists in the favorites section. If it does, the function removes it; if it doesn't.
+ * If the validations go wrong, it shows a message with the response error.
+ * 
+ * @param {String} id The user id.
+ * @param {String} token The token to authorize user validation.
+ * @param {String} gifid The GIF id.
+ * 
+ * @return {Object} Response on the process and modifies favorites array.
+ */
+
+
+
 logic.retrieveFavGifs = function (id, token) {
     validate.string(id, 'id')
     validate.string(token, 'token')
