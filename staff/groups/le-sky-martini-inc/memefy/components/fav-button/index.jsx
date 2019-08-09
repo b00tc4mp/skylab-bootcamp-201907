b3df655e27e24a04dc3f1e4acf9e435dab699290
CarolinaZLin <carolina.z.lin@gmail.com>
2019-08-09 12:04:46 +0200

    fix fav-button error

--- staff/groups/le-sky-martini-inc/memefy/components/fav-button/index.jsx
+++ staff/groups/le-sky-martini-inc/memefy/components/fav-button/index.jsx
@@ -1,11 +1,11 @@
 /**
+ * 
  * Button to mark an item as favorite.
  */
-​
+
 function FavButton({ active, onToggle }) {
     return <button className={`results__favorites`} onClick={event => {
         event.stopPropagation()
-
         onToggle()
-    }}>{active ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}</button>
+    }}>{active ? <i class="fas fa-heart"></i> : <i class="far fa-heart"></i>}</button>
 }
