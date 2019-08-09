b81c08cf05892c12e85e02cd4c63e3bd15cfe152
Adria Arquimbau <adria.arki@gmail.com>
2019-08-09 22:03:15 +0200

    update success login panel DONE

--- staff/groups/over-cook-sa/overcooked/components/register-success.jsx
+++ staff/groups/over-cook-sa/overcooked/components/register-success.jsx
@@ -1,18 +1,12 @@
 function RegisterSuccess({ onLogin }) {
   return (
-    <p>
-      User successfully registered, you can now proceed to{' '}
-      <a
-        href=""
-        onClick={event => {
+    <section className="register-succes">
+    <p> User successfully registered, you can now proceed to{' '}
+      <a className="register-succes__login" href="" onClick={event => {
           event.preventDefault()
 
           onLogin()
-        }}
-      >
-        login
-      </a>
-      .
-    </p>
+        }} > login </a> .</p>
+        </section>
   )
 }
