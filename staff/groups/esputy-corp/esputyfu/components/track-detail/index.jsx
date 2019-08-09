b18eedd08376900d25c406655921a1f5f23c0eb5
Roger Perez Serrano <Roger@MacBook-Air-de-Roger.local>
2019-08-09 10:51:06 +0200

    add style to this fucking awsome app

--- staff/groups/esputy-corp/esputyfu/components/track-detail/index.jsx
+++ staff/groups/esputy-corp/esputyfu/components/track-detail/index.jsx
@@ -1,21 +1,23 @@
 function TrackDetail({ track: { url, favorite, linkAlbum, nameAlbum, releaseDate, linkkArtist, nameArtist, explicit, linkTrack, idTrack, nameTrack, popularity, previewUrl }, onBack, onToggle }) {
     return <>
-        <img src={url} />
-        <h3><a href={linkTrack}>{nameTrack}</a></h3>
+        <div className='container'>
+        <img className='container container__img' src={url} />
+        <h3><a className='container__a' href={linkTrack}>{nameTrack}</a></h3>
         <FavButton active={favorite} onToggle={() => onToggle(idTrack)}/>
         {previewUrl !== null ? <audio controls>
             <source src={previewUrl} type="audio/ogg"></source>
             <source src={previewUrl} type="audio/mp3"></source>
         </audio> : <p>Lo siento spotify no tiene preview de esta canción</p>}
-        <ul>
-            <li>Artista: <a href={linkkArtist} target="_blank" >{nameArtist}</a></li>
-            <li>Album: <a href={linkAlbum} target="_blank" >{nameAlbum}</a></li>
+        <ul className='container__ul--detail'>
+            <li>Artista: <a className='container__a' href={linkkArtist} target="_blank" >{nameArtist}</a></li>
+            <li>Album: <a className='container__a' href={linkAlbum} target="_blank" >{nameAlbum}</a></li>
             <li>Creación: {releaseDate}</li>
         </ul>
-        {onBack && <a href="" onClick={ event => {
+        {onBack && <a className='container__a' href="" onClick={ event => {
             event.preventDefault()
 
             onBack()
         }}>Go Back</a>}
+        </div>
     </>
 }
