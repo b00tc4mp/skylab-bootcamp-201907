2a7bb2e0afd036f335c3ee3a00f78427232fa691
albert fuente homs <example@example.com>
2019-08-12 17:08:44 +0200

    added files

--- staff/albert-fuente/e-duck/1/components/results.jsx
+++
@@ -1,9 +0,0 @@
-function Results(props) {
-    return <ul>
-        {props.items.map(item => <li key={item.id} onClick={()=>{
-            props.onItemClicked(item.id) //captura el click callback que posem a landing
-        }}>
-            {props.paintItem(item)}
-        </li>)}
-    </ul>
-}
