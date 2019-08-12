9613133d6f23f4c10ebc124634941040f9f594fa
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-12 14:13:46 +0200

    update sync io

--- staff/jaume-villarreal/learnyounode/first-async-io/index.js
+++ staff/jaume-villarreal/learnyounode/first-async-io/index.js
@@ -1,17 +1,20 @@
 var fs = require('fs')
 
-const file = process.argv[2].toString()
+const {argv : [ , , file]} = process
 
-const readFile = expression => {
-    fs.readFile( file , (error , data) => {
-        if(error) throw Error (error)
-        else expression(file)
+fs.readFile( file , (error , data) => {
+    if(error) throw Error (error)
+    else{
+        // countLines(data)
+        const BREAK_REGEX = /\r?\n/g
+        const lines = data.match(BREAK_REGEX).length
+        console.log(lines);
+    }
 })
-}
 
-readFile(countLines)
-
-const countLines = file => {
-    const lines = file.split('\n')
-    console.log (lines.length-1)
-}
+// const countLines = (content) => {
+//     const BREAK_REGEX = /\r?\n/g
+//         const lines = data.match(BREAK_REGEX).length
+//         console.log(lines);
+// }
+    
