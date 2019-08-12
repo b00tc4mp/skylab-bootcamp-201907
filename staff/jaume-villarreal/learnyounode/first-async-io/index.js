e9e079214f4d4d5649e5d7bc2b974e0870916b39
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-12 14:44:24 +0200

    update async step

--- staff/jaume-villarreal/learnyounode/first-async-io/index.js
+++ staff/jaume-villarreal/learnyounode/first-async-io/index.js
@@ -1,20 +1,15 @@
 var fs = require('fs')
 
-const {argv : [ , , file]} = process
+const { argv : [ , , file] } = process
 
-fs.readFile( file , (error , data) => {
+fs.readFile( file , 'utf-8' , (error , data) => {
     if(error) throw Error (error)
-    else{
-        // countLines(data)
-        const BREAK_REGEX = /\r?\n/g
-        const lines = data.match(BREAK_REGEX).length
-        console.log(lines);
-    }
+    countLines(data) 
 })
 
-// const countLines = (content) => {
-//     const BREAK_REGEX = /\r?\n/g
-//         const lines = data.match(BREAK_REGEX).length
-//         console.log(lines);
-// }
+const countLines = (content) => {
+    const BREAK_REGEX = /\r?\n/g
+        const lines = content.match(BREAK_REGEX).length
+        console.log(lines);
+}
     
