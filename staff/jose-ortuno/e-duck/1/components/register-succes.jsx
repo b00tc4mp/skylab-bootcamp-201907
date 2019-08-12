1a91d5a15c37385cb1bea68399e6b4faee108d9b
xossse <xossse@MacBook-Pro-de-xossse.local>
2019-08-12 11:53:23 +0200

    my personal version

--- staff/jose-ortuno/e-duck/1/components/register-succes.jsx
+++
@@ -1,8 +0,0 @@
-function RegisterSucces(props) {
-    return <>
-        <p>User successfully registered, you can now proceed to <a href="" onClick={event => {
-            event.preventDefault()
-            props.onLogin()
-        }}>login</a></p>
-    </>
-}
