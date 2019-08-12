6eb8e916b03bd3869cfc394e730b349a7dedf6c7
aramanjon <aracelimanjoncb@gmail.com>
2019-08-12 16:28:41 +0200

    learnyounode

---
+++ staff/ara-manjon/learnyounode/filtered-ls/index.js
@@ -0,0 +1,14 @@
+
+const fs = require('fs')
+const path = require('path') //devuelve la extension del archivo
+const { argv: [,, folder, extension] } = process
+
+
+fs.readdir(folder, function(err, list){
+    if(err) throw err
+    for(i=0; i<list.length; i++){
+        if(path.extname(list[i]) === `.${extension}`) console.log(list[i])    
+    }
+})
+
+
