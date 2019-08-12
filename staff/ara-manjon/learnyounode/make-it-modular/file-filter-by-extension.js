2e33db53926a24aab1c942227d959b83e8961ca6
aramanjon <aracelimanjoncb@gmail.com>
2019-08-12 16:41:00 +0200

    learnyounode exercise 5

--- staff/ara-manjon/learnyounode/make-it-modular/file-filter-by-extension.js
+++ staff/ara-manjon/learnyounode/make-it-modular/file-filter-by-extension.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const path = require('path') 
 
 function filterFilesByExtension(folder, extension, callback){
-fs.readdir(folder, function(err, list){
+fs.readdir(folder, function(err, files){
     if(err) return callback(err)
-    
+
     const filtered = files.filter(file => (path.extname(file[i]) === `.${extension}`))
      
     callback(undefined,filtered)
