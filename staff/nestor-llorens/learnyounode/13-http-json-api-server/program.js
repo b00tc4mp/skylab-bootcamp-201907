f1cee704254a8b46c343f356e87d7e50ce3dd27b
Nestor Llorens <nesllorens@gmail.com>
2019-08-13 17:34:58 +0200

    13 to do

--- staff/nestor-llorens/learnyounode/13-http-json-api-server/program.js
+++ staff/nestor-llorens/learnyounode/13-http-json-api-server/program.js
@@ -1,9 +1,11 @@
 const http = require('http')
-
-let str = ''
+const url = require('url')
 
 const server = http.createServer((req, res) => {
+    const parsedPath = url.parse(req.url, true)
+    const parsedQuery = url.parse(req.url, true)
 
-       
+    
+
 })
 server.listen(process.argv[2])
