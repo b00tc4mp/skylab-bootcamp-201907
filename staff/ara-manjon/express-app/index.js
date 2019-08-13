13f9a6d5ac8a2678ba3e69b606ca5346e67696cc
aramanjon <aracelimanjoncb@gmail.com>
2019-08-13 22:20:09 +0200

    e-duck server

--- staff/ara-manjon/express-app/index.js
+++ staff/ara-manjon/express-app/index.js
@@ -1,13 +1,38 @@
+const http= require('http')
 const express = require('express')
-debugger
-const {argv:[,, port]}= process
+const { argv: [, , port] } = process
 const app = express()
+// http://localhost:8080/ => <form><input name="query">...</form>
 
-
-app.get('/', (red, req)=>{
-    
+app.get('/', (req, res) => {
     res.send(`<form action="/search">
-    <input></input>
-    `)
+        <input type="text" name="q">
+        <button>Search</button>
+    </form>`)
+})
+
+app.get('/search', (req, res) => {
+    const {query:{q} }= req
+    
+    const url= `http://duckling-api.herokuapp.com/api/search?q=${q}`
+    http.get(url, response=>{
+        let content= ''
+        response.on('data', chunk => content += chunk)
+        response.on('end', ()=> {
+            const ducksJson = JSON.parse(content)
+            const ducks= ducksJson.map(duck=>{
+                const { title, imageUrl }= duck
+                
+                return `<li>
+                <h3>${duck.title}</h3>
+                
+                <img src=${duck.imageUrl} />
+                </li>`
+            })
+            res.send(`<ul>${ducks.join('')}</ul>`)
+    })
+    
+})
 
 })
+app.listen(port)
