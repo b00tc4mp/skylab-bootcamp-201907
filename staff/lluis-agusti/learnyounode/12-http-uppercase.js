953aa8c928fc69314de7d83d0165aa1b95dcf1c1
Lluis <lluisagustisan@gmail.com>
2019-08-13 15:41:18 +0200

    update commit

--- staff/lluis-agusti/learnyounode/12-http-uppercase.js
+++ staff/lluis-agusti/learnyounode/12-http-uppercase.js
@@ -1,13 +1,13 @@
 var http = require('http')
 var map = require('through2-map')
-
-const { argv :[ ,,url]}= process;
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST')
-  res.writeHead(200, { 'content-type': 'text/plain' })
  
-  fs.createWriteStream(ext).pipe(res.toString().toUppercase())
+var server = http.createServer(function (req, res) {
+ if (req.method != 'POST')
+   return res.end('send me a POST\n')
+ 
+ req.pipe(map(function (chunk) {
+   return chunk.toString().toUpperCase()
+ })).pipe(res)
 })
  
-server.listen(Number(folder))
+server.listen(Number(process.argv[2]))
