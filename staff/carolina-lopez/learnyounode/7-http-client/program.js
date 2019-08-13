3c52aa90063b6e9c12730ea2d476b5c93567cfa2
Carolina Lopez Casanova <carolinalopezcasanova@MacBook-Pro-de-Carolina.local>
2019-08-13 10:49:16 +0200

    add learnyounode

---
+++ staff/carolina-lopez/learnyounode/7-http-client/program.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+const { argv : [,, url]} = process
+
+http.get(url, response => {
+  response.setEncoding('utf8')
+
+  response.on('error', error => { throw error})
+
+  response.on('data', content => console.log(content))
+})
--- staff/carolina-lopez/learnyounode/make-it-modular/program.js
+++
@@ -1,6 +0,0 @@
-var mymodule = require('./mymodule.js')
-
-const { argv : [,, dir, ext] } = process
-
-mymodule(dir, ext, files => files.forEach(file => console.log(file)))
- 
