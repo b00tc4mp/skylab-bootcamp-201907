5dd1ba94d2cb3b27105fe6811f76da34e6939ef6
albert fuente homs <example@example.com>
2019-08-13 16:49:03 +0200

    added files

--- staff/albert-fuente/learnyounode/make-it-modular/filter-files-byextension.js
+++ staff/albert-fuente/learnyounode/make-it-modular/filter-files-byextension.js
@@ -1,16 +1,16 @@
-const fs=require("fs")
-const path=require("path")
+const fs = require('fs')
+const path = require('path')
 
-function filterFilesByExtension(folder,extension,callback){
-    fs.readdir(folder,(error,files)=>{
-        if(error) return callback(error)
+function filterFilesByExtension(folder, extension, callback) {
+    fs.readdir(folder, (error, files) => {
+        if (error) return callback(error)
 
-        const _extension=`.${extension}`
+        const _extension = `.${extension}`
 
-        const filtered=files.filter(file=>path.extname(file)===_extension)
+        const filtered = files.filter(file => path.extname(file) === _extension)
 
-        callback(undefined,filtered)
+        callback(undefined, filtered)
     })
 }
 
-module.exports=filterFilesByExtension
+module.exports = filterFilesByExtension
--- staff/albert-fuente/learnyounode/make-it-modular/index.js
+++ staff/albert-fuente/learnyounode/make-it-modular/index.js
@@ -1,9 +1,9 @@
-const FilterFilesByExtension=require("./filter-files-byextension")
+const filterFilesByExtension = require('./filter-files-byextension')
 
-const{argv:[,,folder,extension]}=process 
+const { argv: [, , folder, extension] } = process
 
-FilterFilesByExtension(folder,extension,(error,files)=>{
-    if(error) throw error 
+filterFilesByExtension(folder, extension, (error, files) => {
+    if (error) throw error
 
-    files.forEach(file=>console.log(file))
+    files.forEach(file => console.log(file))
 })
