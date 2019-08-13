f973fba85c30eefc69b0568286a6f6ad38556c3d
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-13 09:50:25 +0200

    complete learnyounode

--- staff/jaume-villarreal/learnyounode/12-http-json-api-server/index.js
+++ staff/jaume-villarreal/learnyounode/12-http-json-api-server/index.js
@@ -2,20 +2,36 @@ const http = require('http')
 let url = require('url')
 const { argv : [ , , port] } = process
 
-const getTime = () => {
-    return{
-        hour: getHour
+
+const server = http.createServer((request , response) => {
+    if(request.method === 'GET'){
+        response.writeHead(200 , {'content-type' : 'application/json'})
+
+        //parse URL => acces to URL Object properties
+        let parsedURL = url.parse(request.url , true)
+        
+        // retrieve API's path
+        let path = parsedURL.pathname
+
+        // retrieve date from URL's Query Object => property 'iso'
+        let date = new Date(parsedURL.query.iso)
+
+        let output
+
+        if(path === '/api/parsetime'){
+            output = {
+                hour : date.getHours(),
+                minute : date.getMinutes(),
+                second : date.getSeconds()
+            }
+        }else if(path==='/api/unixtime'){
+            output = { unixtime : date.getTime() }
+        }
+        response.end(JSON.stringify(output))
+    }else{
+        response.writeHead(405)
     }
-}
-
-const server = http.createServer( (request , response) =>{
-    url = url.parse(request.url , true)
-    console.log(url.path)
 }).listen(port)
-
-
-
-
 
 
 
