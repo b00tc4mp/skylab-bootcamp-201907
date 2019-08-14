95df614d606ef5a32404387367ca4e6e319f6d28
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-08-14 13:36:05 +0200

    update

--- staff/juan-bovo/express-app/index.js
+++ staff/juan-bovo/express-app/index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/search', (req, res) => {
     // res.send(`ok, searching... ${req.query.q}`)
-    const url = `duckling-api.herokuapp.com/api/search?q=${req.query.q}`
+    const url = `http://duckling-api.herokuapp.com/api/search?q=${req.query.q}`
     //const url = `http://duckling-api.herokuapp.com/api/search?q=${req.query.q}` //--> Yo puse HTTP
     http.get(url, response => {
         let content = ''
@@ -22,14 +22,35 @@ app.get('/search', (req, res) => {
         response.on('end', () => {
             const jsonData = JSON.parse(content)
             const ducks = jsonData.map(duck => {
-                const { title, imageUrl, price } = duck
-                return `<li>
+                const { title, imageUrl, price, id } = duck
+                return `<a href="/detail/${id}/"><li>
                   <h2>${title}</h2>
                   <img src=${imageUrl} />
                   <span>${price}</span>
-                  </li>`
+                  </li></a>`
             })
             res.send(`<ul>${ducks.join('')}</ul>`)
+        })
+    })
+})
+
+app.get('/detail/:id', (req, res) => {
+    let data = ''
+    http.get(`http://duckling-api.herokuapp.com/api/ducks/${req.params.id}`, response => {
+        response.on('error', error => res.send(error.message))
+        response.on('data', chunk => data += chunk)
+        response.on('end', () => {
+            const jsonData = JSON.parse(data)
+            if (jsonData.error) throw new Error(jsonData.error)
+            const { title, imageUrl, price, description, link } = jsonData
+            const htmlResponse = `
+                    <h2>${title}</h2>
+                    <img src=${imageUrl}></img>
+                    <p>${description}</p>
+                    <p>${price}</p>
+                    <a href=${link} target="_blank">Buy this ducks on STORE</a>
+                    `
+            res.send(htmlResponse)
         })
     })
 })
