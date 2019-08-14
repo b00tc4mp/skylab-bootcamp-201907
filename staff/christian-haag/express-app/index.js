ba6f7f8719fd7dcdc2e210081a3489024537f62b
vanhaaggen <vanhaaggen@gmail.com>
2019-08-14 08:50:35 +0200

    add express-app

--- staff/christian-haag/express-app/index.js
+++ staff/christian-haag/express-app/index.js
@@ -1,1 +1,44 @@
 const express = require('express')
+const http = require('http')
+const fs = require('fs')
+const { argv: [, , port] } = process
+const app = express()
+
+
+// http://localhost:8080/ => <form><input name="query">...</form>
+app.get('/', (req, res) => {
+    res.send(`<form action="/search">
+        <input type="text" name="q">
+        <button>Search</button>
+    </form>`)
+})
+
+
+
+app.get('/search', (req, response) => {
+    const query = req.query.q
+    let data = ''
+
+    http.get(`http://duckling-api.herokuapp.com/api/search?q=${query}`, res => {
+        res.on('data', chunk => data += chunk)
+        res.on('end', () => {
+            const json = JSON.parse(data)
+            debugger
+            let list = json.map(item => {
+                const { title, imageUrl, price } = item
+                return `<li>
+                <h2>${title}</h2>
+                <img src=${imageUrl} />
+                <p>${price}</p>
+                </li>`
+            })
+            response.send(`<ul>${list.join('')}</ul>`)
+        })
+        res.on('error', error => { response.send(error) })
+    })
+
+})
+app.listen(port)
+
+
+
