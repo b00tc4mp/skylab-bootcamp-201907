de38bb347b928cba78211aecf58fe67d57c1be74
Oihane <oihaneuncitig@gmail.com>
2019-08-14 22:40:59 +0200

    finish exercise

--- staff/oihane-unciti/learnyounode/baby/index.js
+++ staff/oihane-unciti/learnyounode/baby/index.js
@@ -1,13 +1,1 @@
-
-
-var sum = process.argv.reduce(function (a, b, c) {
-  if (c >= 2 && !isNaN(b)) {
-    return +a + +b
-  } else if (c >= 2) {
-     return +a + 0;
-  } else {
-     return 0;
-  }
-})
- 
-console.log(sum)
+console.log(process.argv.slice(2).reduce((accumulator, currentValue) => Number(accumulator) + Number(currentValue)))
