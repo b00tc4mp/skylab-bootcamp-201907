c26d38bb0e436b55362d6db2f238f233f1e0d54c
Lluis <lluisagustisan@gmail.com>
2019-08-14 13:25:34 +0200

    update con ducks wed 14th

--- staff/lluis-agusti/express-app/index.js
+++ staff/lluis-agusti/express-app/index.js
@@ -9,6 +9,33 @@ app.get('', (req, res) => {
         <button>Search</button>
     </form>`)
 })
+
+function renderHtml() {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+  </head>
+  <body>
+      
+  </body>
+  </html>`
+}
+
+function renderSearch() {
+  return `<header>
+  ${content}
+  </header>`
+}
+
+function renderHeader(content) {
+  return `<header>
+  ${content}
+  </header>`
+}
 app.get('/search', (req, res) => {
     //  res.send(`ok, searching... ${req.query.q}`)
     const url = `http://duckling-api.herokuapp.com/api/search?q=${req.query.q}`
@@ -18,19 +45,42 @@ app.get('/search', (req, res) => {
       response.on('end', () => {
         const jsonData = JSON.parse(content)
 
-
-
         const ducks = jsonData.map(duck => {
-          const {title, imageUrl, price} = duck
-          return `<li>
+          const {title, imageUrl, price, id} = duck
+          return `<li><a href="">
             <h2>${title}</h2>
             <img src=${imageUrl} />
             <span>${price}</span>
+            <span>${id}</span></a>
             </li>`
         })
         res.send(`<ul>${ducks.join('')}</ul>`)
       })
     })
-    // TODO call duckling api endpoint that searches ducks, wait for the answer and the return ducks in <UL><LI>...
 })
+
+app.get('/duck/:id', (req, res) => {
+    const { params: {id } }= req
+
+    const url = `http://duckling-api.herokuapp.com/api/ducks${id}`
+    http.get(url, response => {
+      let content = ''
+      response.on('data', chunk => content += chunk)
+      response.on('end', () => {
+        const duck = JSON.parse(content)
+        // const duck = jsonData.map(duck => {
+
+          const {title, imageUrl, price, description, link} = duck
+          
+          return `<li>
+            <h2>${title}</h2>
+            <img src=${imageUrl} />
+            <span>${price}</span>
+            <p>${description}</p>
+            <a href="${link}" target="_blank">Link to Store</a>
+            </li>`
+        })
+        res.send(`<ul>${duck}</ul>`)
+}
+
 app.listen(port)
