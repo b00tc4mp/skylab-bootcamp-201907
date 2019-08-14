d5c3e712c8853a28f85641a4a530043e115fa08e
joseortuno <joseantortunorioja@gmail.com>
2019-08-14 14:46:27 +0200

    update detail

--- staff/jose-ortuno/express-app/index.js
+++ staff/jose-ortuno/express-app/index.js
@@ -7,7 +7,6 @@ const app = express()
 app.get('/', (req, res) => {
     res.send(`<form action="/search">
         <input type="text" name="q">
-        <input type="text" name="carolina">
         <button>Search</button>
     </form>`)
 })
@@ -24,22 +23,51 @@ app.get('/search', (req, res) => {
     const url = `http://duckling-api.herokuapp.com/api/search?q=${q}`
     const request = http.get(url, response => {
         let content = ''
-        response.on('error', error => {throw new Error(error)})
+        response.on('error', error => { throw new Error(error) })
         response.on('data', chunk => content += chunk)
         response.on('end', () => {
             const jsonData = JSON.parse(content)
-            const ducks = jsonData.map(({ title, imageUrl, price }) =>
+            const ducks = jsonData.map(({ title, imageUrl, price, id }) =>
                 `<li>
-                    <h2>${title}</h2>
-                    <img src=${imageUrl} />
-                    <span>${price}</span>
+                    <a href=/duck/${id}>
+                        <h2>${title}</h2>
+                        <img src="${imageUrl}" />
+                        <span>${price}</span>
+                    </a>
                 </li>`
             )
 
             res.send(`<ul>${ducks.join('')}</ul>`)
         })
 
-    }).on('error', error => {throw new Error(error)})
+    }).on('error', error => { throw new Error(error) })
+
+    request.on('error', error => { throw error })
+
+})
+
+app.get('/duck/:id', (req, res) => {
+    const { params: { id } } = req
+    const url = `http://duckling-api.herokuapp.com/api/ducks/${id}`
+    const request = http.get(url, response => {
+        let content = ''
+        response.on('error', error => { throw new Error(error) })
+        response.on('data', chunk => content += chunk)
+        response.on('end', () => {
+            const jsonData = JSON.parse(content)
+
+            const { title, imageUrl, price, description, link } = jsonData
+
+            res.send(`<section>
+                <h1>${title}</h1>
+                <img src="${imageUrl}" />
+                <span>${price}</span>
+                <p>${description}</p>
+                <a href=${link} target="_blank">Go to store</a>
+            </section>`)
+        })
+
+    }).on('error', error => { throw new Error(error) })
 
     request.on('error', error => { throw error })
 
