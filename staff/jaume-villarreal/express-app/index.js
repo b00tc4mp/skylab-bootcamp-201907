e7303409ca670bfdb240c621ece26b049c5bad22
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-14 13:39:44 +0200

    update express-app

--- staff/jaume-villarreal/express-app/index.js
+++ staff/jaume-villarreal/express-app/index.js
@@ -4,30 +4,18 @@ const http = require('http')
 
 const { argv : [ , , port]} = process
 
-const endPointDucks = "http://duckling-api.herokuapp.com/api/search?q="
-
-
-// const form =    render(`<form action="/search">
-//                     <input type= text name="q" id="q"
-//                     <br/>
-//                     <button>Search</button>
-//                 </form>`)
-
-const form =    `<form action="/search">
-                    <input type= text name="q" id="q"
-                    <br/>
-                    <button>Search</button>
-                </form> `
-
+const endPointSearchDucks = "http://duckling-api.herokuapp.com/api/search?q="
+const endPointDetailDuck = "http://duckling-api.herokuapp.com/api/ducks/"
 
 app.get('/' , (request , response) => {
-    response.send(form)
+    response.send(renderSearch())
 })
 
 app.get('/search' , (request , response) => {
+
     const { query:{ q:search }} = request
 
-    const httpRequest = http.get(`${endPointDucks}${search}` , res => {
+    const httpRequest = http.get(`${endPointSearchDucks}${search}` , res => {
         res.on('error' , error => {throw error})
         
         let ducks = ''
@@ -39,7 +27,29 @@ app.get('/search' , (request , response) => {
 
             if(ducks.error) throw new Error(ducks.error)
 
-            response.send(paintListDucks(ducks)).on('error' , error => {throw error})
+            response.send(renderListDucks(ducks , search)).on('error' , error => {throw error})
+        })
+    })
+    httpRequest.on('error' , error => { throw error })
+})
+
+app.get('/ducks/:idDuck' , (request , response) => {
+    
+    const { params : { idDuck } } = request
+
+    const httpRequest = http.get(`${endPointDetailDuck}${idDuck}` , res => {
+        res.on('error' , error => {throw error})
+        
+        let duck = ''
+
+        res.on('data' , chunk => duck += chunk )
+        
+        res.on('end' , () => {
+            duck = JSON.parse(duck)
+
+            if(duck.error) throw new Error(duck.error)
+
+            response.send(renderDetailDuck(duck)).on('error' , error => {throw error})
         })
     })
     httpRequest.on('error' , error => { throw error })
@@ -47,8 +57,8 @@ app.get('/search' , (request , response) => {
 
 app.listen(port)
 
-const render = body => {
-    return `<!DOCTYPE html>
+function render(body){
+    return  `<!DOCTYPE html>
                 <html lang="en">
                     <head>
                         <meta charset="UTF-8">
@@ -62,15 +72,37 @@ const render = body => {
                 </html>`
 }
 
-const paintListDucks = ducks => {
-    let urlString = `<a href="/">BACK</a><ul>`
+function renderSearch(){
+    return `<form action="/search">
+                    <input type= text name="q" id="q"
+                    <br/>
+                    <button>Search</button>
+                </form> `
+}   
+
+const renderListDucks = (ducks , search) => {
+    let urlString = `<a href="/">BACK</a><ul><span>${search}</span>`
     ducks.forEach( duck => {
         urlString +=    `<li>
-                            <h2>${duck.title}</h2>  
-                            <img src='${duck.imageUrl}'/>  
-                            <span>${duck.price}</span>
+                            <a href="/ducks/${duck.id}">
+                                <h2>${duck.title}</h2>  
+                                <img src='${duck.imageUrl}'/>  
+                                <span>${duck.price}</span>
+                            </a>
                         </li>`
     })
     urlString += '</ul>'
+    return urlString
+}
+
+const renderDetailDuck = duck => {
+    let urlString = `<a href="search/">BACK</a><ul>`
+        urlString +=    `<article>
+                            <h2>${duck.title}</h2>  
+                            <img src='${duck.imageUrl}'/>  
+                            <p>${duck.description}</p>
+                            <span>${duck.price}</span>
+                            <a href="${duck.link}" target="_blank">Go to store</a>
+                        </article>`
     return urlString
 }
