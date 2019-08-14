e91ceb73a5eb1047a600fc322269ff94dd94e310
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-08-14 10:33:20 +0200

    search ducks returns ducks

--- staff/juan-bovo/express-app/index.js
+++ staff/juan-bovo/express-app/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const http = require('http')
 const { argv: [, , port] } = process
 
 const app = express()
@@ -13,9 +13,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-    res.send(`ok, searching... ${req.query.q}`)
-
-    // TODO call duckling api endpoint that searches ducks, wait for the answer and the return ducks in <UL><LI>...
+    // res.send(`ok, searching... ${req.query.q}`)
+    const url = `duckling-api.herokuapp.com/api/search?q=${req.query.q}`
+    //const url = `http://duckling-api.herokuapp.com/api/search?q=${req.query.q}` //--> Yo puse HTTP
+    http.get(url, response => {
+        let content = ''
+        response.on('data', chunk => content += chunk)
+        response.on('end', () => {
+            const jsonData = JSON.parse(content)
+            const ducks = jsonData.map(duck => {
+                const { title, imageUrl, price } = duck
+                return `<li>
+                  <h2>${title}</h2>
+                  <img src=${imageUrl} />
+                  <span>${price}</span>
+                  </li>`
+            })
+            res.send(`<ul>${ducks.join('')}</ul>`)
+        })
+    })
 })
 
 app.listen(8000)
