89c29a3568cdb9afb74c8f5e1490a11b61d11f1d
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-14 17:07:43 +0200

    update index.js

--- staff/jaume-villarreal/express-app/index.js
+++ staff/jaume-villarreal/express-app/index.js
@@ -4,11 +4,13 @@ const http = require('http')
 
 const { argv : [ , , port]} = process
 
+const {Html , Header , Search , DuckResults , DuckDetail} = require('./components')
+
 const endPointSearchDucks = "http://duckling-api.herokuapp.com/api/search?q="
 const endPointDetailDuck = "http://duckling-api.herokuapp.com/api/ducks/"
 
 app.get('/' , (request , response) => {
-    response.send(renderSearch())
+    response.send(Html(Search()))
 })
 
 app.get('/search' , (request , response) => {
@@ -27,8 +29,8 @@ app.get('/search' , (request , response) => {
 
             if(ducks.error) throw new Error(ducks.error)
 
-            response.send(renderListDucks(ducks , search)).on('error' , error => {throw error})
-        })
+            response.send(Html(`${Search()}${DuckResults(ducks)}`)).on('error' , error => {throw error})
+        })  
     })
     httpRequest.on('error' , error => { throw error })
 })
@@ -49,60 +51,10 @@ app.get('/ducks/:idDuck' , (request , response) => {
 
             if(duck.error) throw new Error(duck.error)
 
-            response.send(renderDetailDuck(duck)).on('error' , error => {throw error})
+            response.send(Html(`${Search()}${DuckDetail(duck)}`)).on('error' , error => {throw error})
         })
     })
     httpRequest.on('error' , error => { throw error })
 })
 
 app.listen(port)
-
-function render(body){
-    return  `<!DOCTYPE html>
-                <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                        <title>Duck Express</title>
-                    </head>
-                    <body>
-                        ${body}
-                    </body>
-                </html>`
-}
-
-function renderSearch(){
-    return `<form action="/search">
-                    <input type= text name="q" id="q"
-                    <br/>
-                    <button>Search</button>
-                </form> `
-}   
-
-const renderListDucks = (ducks , search) => {
-    let urlString = `<a href="/">BACK</a><ul><span>${search}</span>`
-    ducks.forEach( duck => {
-        urlString +=    `<li>
-                            <a href="/ducks/${duck.id}">
-                                <h2>${duck.title}</h2>  
-                                <img src='${duck.imageUrl}'/>  
-                                <span>${duck.price}</span>
-                            </a>
-                        </li>`
-    })
-    urlString += '</ul>'
-    return urlString
-}
-
-const renderDetailDuck = duck => {
-    let urlString = `<a href="search/">BACK</a><ul>`
-        urlString +=    `<article>
-                            <h2>${duck.title}</h2>  
-                            <img src='${duck.imageUrl}'/>  
-                            <p>${duck.description}</p>
-                            <span>${duck.price}</span>
-                            <a href="${duck.link}" target="_blank">Go to store</a>
-                        </article>`
-    return urlString
-}
