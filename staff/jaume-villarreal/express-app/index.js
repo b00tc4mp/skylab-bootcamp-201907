259d916b8b03d211a1370d54f390949aea7c9976
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-14 10:28:44 +0200

    update express-app

--- staff/jaume-villarreal/express-app/index.js
+++ staff/jaume-villarreal/express-app/index.js
@@ -4,13 +4,19 @@ const http = require('http')
 
 const { argv : [ , , port]} = process
 
-const endpointDucks = "http://duckling-api.herokuapp.com/api/search?q="
+const endPointDucks = "http://duckling-api.herokuapp.com/api/search?q="
 
+
+// const form =    render(`<form action="/search">
+//                     <input type= text name="q" id="q"
+//                     <br/>
+//                     <button>Search</button>
+//                 </form>`)
 
 const form =    `<form action="/search">
                     <input type= text name="q" id="q"
                     <br/>
-                    <button>Search<b/utton>
+                    <button>Search</button>
                 </form> `
 
 
@@ -21,30 +27,48 @@ app.get('/' , (request , response) => {
 app.get('/search' , (request , response) => {
     const { query:{ q:search }} = request
 
-    http.get(endpointDucks + search , res => {
+    const httpRequest = http.get(`${endPointDucks}${search}` , res => {
         res.on('error' , error => {throw error})
         
         let ducks = ''
+
         res.on('data' , chunk => ducks += chunk )
         
         res.on('end' , () => {
             ducks = JSON.parse(ducks)
-            response.send(listDucks(ducks))
-            .on('error' , error => {throw error})
+
+            if(ducks.error) throw new Error(ducks.error)
+
+            response.send(paintListDucks(ducks)).on('error' , error => {throw error})
         })
     })
+    httpRequest.on('error' , error => { throw error })
 })
 
 app.listen(port)
 
+const render = body => {
+    return `<!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <title>Duck Express</title>
+                    </head>
+                    <body>
+                        ${body}
+                    </body>
+                </html>`
+}
 
-const listDucks = ducks => {
-    let urlString = '<ul>'
+const paintListDucks = ducks => {
+    let urlString = `<a href="/">BACK</a><ul>`
     ducks.forEach( duck => {
         urlString +=    `<li>
                             <h2>${duck.title}</h2>  
                             <img src='${duck.imageUrl}'/>  
-                            <p>${duck.price}</p>
+                            <span>${duck.price}</span>
                         </li>`
     })
     urlString += '</ul>'
