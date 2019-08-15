c8a7a212adee15c70acaf2db05782d6a526ca523
Felipe <felipefcor@gmail.com>
2019-08-15 13:18:13 +0200

    version with register and login working

---
+++ staff/felipe-fernandez/express-app/myapp/components/login.js
@@ -0,0 +1,10 @@
+function Login() {
+    return `<h1>Login</h1>
+             <form method="POST" action="/login">
+                 <label>E-mail<input type="email" name="email" /></label>
+                 <label>Password<input type="password" name="password" /></label>
+            <button>Login</button>
+        </form>`
+ }
+ 
+ module.exports = Login 
---
+++ staff/felipe-fernandez/express-app/myapp/logic/index.js
@@ -0,0 +1,7 @@
+const logic = {}
+
+module.exports = {
+    authenticateUser: require('./authenticate-user'),
+    registerUser: require('./register-user'),
+    retrieveUser: require('./retrieve-user'),
+}
