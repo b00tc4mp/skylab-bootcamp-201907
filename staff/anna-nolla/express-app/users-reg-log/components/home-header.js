81735f0545db5ebbf60b5f6c7982cd8ef97ec0b2
Anna <daeneris88@gmail.com>
2019-08-15 13:17:18 +0200

    users and log

---
+++ staff/anna-nolla/express-app/users-reg-log/components/home-header.js
@@ -0,0 +1,9 @@
+function HomeHeader() {
+    return `<header>
+    <ul>
+        <li><a href="/goToLanding">Log Out</a></li>
+        <li><a href="/goToFav">Favorites</a></li>
+    </ul>
+    </header>`
+}
+module.exports = HomeHeader
---
+++ staff/anna-nolla/express-app/users-reg-log/components/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Html: require('./html'),
+    Header: require('./header'),
+    Search: require('./search'),
+    DuckResults: require('./duck-results'),
+    DuckDetail: require('./duck-detail'),
+    Register: require('./register'),
+    Login: require('./login'),
+    HomeHeader: require(`./home-header`),
+    FavButton: require(`./favbutton`)
+
+}
