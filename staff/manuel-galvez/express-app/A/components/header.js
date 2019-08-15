e78fe558a40f83def1a0365021ef805116615b92
lolo <lolo@lolo>
2019-08-15 13:17:47 +0200

    add my version to A folder

---
+++ staff/manuel-galvez/express-app/A/components/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return `<header>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="">FavoritesIfLogged</a></li>
+    </ul>
+    </header>`
+}
+module.exports = Header
