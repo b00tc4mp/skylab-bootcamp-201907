3659f28a664068127768ea03ecd1a1cd7cac4ab8
Sylvain <legrills@gmail.com>
2019-08-17 18:22:57 +0200

    fix duck details

--- staff/sylvain-legrill/express-app/components/html.js
+++ staff/sylvain-legrill/express-app/components/html.js
@@ -6,7 +6,7 @@ function Html(content) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
                 <title>Document</title>
-                <link rel="stylesheet" href="style.css">
+                <link rel="stylesheet" href="/style.css">
                 <link rel="icon" href="favicon.ico" type="image/x-icon" />
             </head>
             <body>
