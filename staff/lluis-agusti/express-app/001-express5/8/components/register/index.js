1b379f6ea71c89101f22f7f1588c5b47ca4eb899
Lluis <lluisagustisan@gmail.com>
2019-08-17 08:36:58 +0200

    update saturday morning

--- staff/lluis-agusti/express-app/001-express5/8/components/register/index.js
+++ staff/lluis-agusti/express-app/001-express5/8/components/register/index.js
@@ -6,12 +6,22 @@ function Register(lang) {
 
     return `<h1>${title}</h1>
         <form method="post" action="${path}">
-            <label>${name}<input type="text" name="name" /></label>
-            <label>${surname}<input type="text" name="surname" /></label>
-            <label>E-mail<input type="email" name="email" /></label>
-            <label>${password}<input type="password" name="password" /></label>
-            <label>${repassword}<input type="password" name="repassword" /></label>
+        
+            <label>
+            ${name}
+            <input type="text" name="name" /></label>
+            <label>
+            ${surname}
+            <input type="text" name="surname" /></label>
+            <label>
+            E-mail
+            <input type="email" name="email" /></label>
+            <label>
+            ${password}<input type="password" name="password" /></label>
+            <label>
+            ${repassword}<input type="password" name="repassword" /></label>
             <button>${title}</button>
+
         </form>
         <a href="">${goback}</a>`
 }
