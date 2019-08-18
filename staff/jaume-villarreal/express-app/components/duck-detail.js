c1ed2cca25571fc4bbfd3133a212d79a921535db
Jaume Villarreal <jaume@macbook-air-2.home>
2019-08-18 18:59:08 +0200

    update styles for dusk detail

--- staff/jaume-villarreal/express-app/components/duck-detail.js
+++ staff/jaume-villarreal/express-app/components/duck-detail.js
@@ -1,13 +1,13 @@
 const FavButton = require('./fav-button')
 
 function DuckDetail({ id, favorite, title, imageUrl, price, description, link }) {
-    return `<article>
-        <h3>${title}</h3>
-        <img src="${imageUrl}">
-        <span>${price}</span>
-        <p>${description}</p>
-        <a href="${link}" target="_blank">Go to store</a>
+    return `<article class="list-item">
+        <h3 class="list-item__header">${title}</h3>
+        <img  class="list-item__image" src="${imageUrl}">
+        <p class="list-item__description">${description}</p>
         ${FavButton(id, favorite)}
+        <span class="list-item__price">${price}</span>
+        <a class="btn" href="${link}" target="_blank">Go to store</a>
     </article>`
 }
 
