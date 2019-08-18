0a9b2aeac02866d6b0f061157573cfea32326b41
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-08-18 10:23:00 +0200

    update duck detail error

--- staff/jaume-villarreal/express-app/components/html.js
+++ staff/jaume-villarreal/express-app/components/html.js
@@ -6,7 +6,7 @@ function Html(content) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
                 
-                <link rel="stylesheet" href="style.css" type="text/css">
+                <link rel="stylesheet" href="/style.css" type="text/css">
                 <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Titillium+Web&display=swap" rel="stylesheet">
                 <link rel="icon" href="favicon.ico" type="image/x-icon" />
                
