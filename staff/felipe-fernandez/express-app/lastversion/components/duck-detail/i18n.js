bd97409fc41298a5822da8ce5ea8f5e5014c346a
Felipe <felipefcor@gmail.com>
2019-08-18 12:01:11 +0200

    last version with favorites, goback and some css

---
+++ staff/felipe-fernandez/express-app/lastversion/components/duck-detail/i18n.js
@@ -0,0 +1,6 @@
+module.exports = {
+    en: { goBack: 'Go back'},
+    es: { goBack: 'Volver'},
+    ca: { goBack: 'Torna'},
+    fr: { goBack: 'Retourner'}
+}
---
+++ staff/felipe-fernandez/express-app/lastversion/components/login/index.js
@@ -0,0 +1,15 @@
+const literals = require('./i18n')
+const { path, goBackPath } = require('./config')
+
+module.exports = function (lang) {
+    const { signIn, goBack, password } = literals[lang]
+
+    return `<h1>${signIn}</h1>
+        <form class="form login" method="post" action="${path}">
+            <label>E-mail<input type="email" name="email" /></label>
+            <label>${password}<input type="password" name="password" /></label>
+            <button>${signIn}</button>
+            <a href="${goBackPath}">${goBack}</a>
+        </form>`
+        
+}
