e574b1167ab8478260064dda23b8f22fa7b3c1c1
Jaume Villarreal <jaume@macbook-air-2.home>
2019-08-19 21:58:36 +0200

    update README.md and favicon path in html.js file

--- staff/jaume-villarreal/express-app/components/html.js
+++ staff/jaume-villarreal/express-app/components/html.js
@@ -8,7 +8,7 @@ function Html(content) {
                 
                 <link rel="stylesheet" href="/style.css" type="text/css">
                 <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Titillium+Web&display=swap" rel="stylesheet">
-                <link rel="icon" href="favicon.ico" type="image/x-icon" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                
                 <title>Document</title>
                
