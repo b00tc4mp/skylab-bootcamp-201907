bbc072e8601109fe47443a010c7b36a2e7064bcd
Oihane <oihaneuncitig@gmail.com>
2019-08-20 22:38:43 +0200

    project

--- staff/oihane-unciti/express-app/components/duck-detail.js
+++ staff/oihane-unciti/express-app/components/duck-detail.js
@@ -1,10 +1,10 @@
 const FavButton = require('./fav-button')
 
 function DuckDetail({ id, favorite, title, imageUrl, price, description, link }) {
-    return `<article class=detail-duck>
-        <h3>${title}</h3>
-        <img src="${imageUrl}">
-        <span>${price}</span>
+    return `<article class="detail-duck">
+        <h3 class="duck-item__title">${title}</h3>
+        <img class="duck-item__image" src="${imageUrl}">
+        <span class="duck-item__price">${price}</span>
         <p>${description}</p>
         <a href="${link}" target="_blank">Go to store</a>
         ${FavButton(id, favorite)}
--- staff/oihane-unciti/express-app/components/header/index.js
+++ staff/oihane-unciti/express-app/components/header/index.js
@@ -7,15 +7,15 @@ function Header(name, query, lang) {
     const { hello, signUp, signIn, signOut, favorite } = literals[lang]
 
     return `<header class="header">
-       <article class = "header__nav">
-        ${LangSelector()}
-        ${name && `<nav>
+       <article class = "header__nav"> 
+       ${LangSelector()}
+        ${name && `<nav class="nav__menu">
             
                 <ul class="menu"><li><a class="nav__a" href="${favoritePath}">${favorite}</a></li>
-                <li><form method="post" action="${signOutPath}"><button class="nav__a">${signOut}</button></form></li></ul>
+                <li><form method="post" action="${signOutPath}"><button class="nav__a button">${signOut}</button></form></li></ul>
                 
             </nav>
-            <h1>${hello}, ${name}!<h1>` || `<nav>
+            <h1 class="hidden">${hello}, ${name}!</h1>` || `<nav class= "nav__menu">
                 <ul class="menu">
                     <li><a class="nav__a" href="${signUpPath}">${signUp}</a></li>
                     <li><a class="nav__a" href="${signInPath}">${signIn}</a></li>
