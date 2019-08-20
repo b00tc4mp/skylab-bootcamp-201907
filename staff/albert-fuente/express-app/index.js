96d0aad98dae128fdc34392a49722a9ffb6468f1
albert fuente homs <example@example.com>
2019-08-20 16:38:18 +0200

    added file

--- staff/albert-fuente/express-app/index.js
+++ staff/albert-fuente/express-app/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Html, Header, DuckResults, DuckDetail, Register, RegisterSuccess, Login } = require('./components')
+const { Html, Header, DuckResults, DuckDetail, Register, RegisterSuccess, Login, feedback } = require('./components')
 const session = require('express-session')
 const logic = require('./logic')
 const literals = require('./constants')
@@ -32,9 +32,11 @@ app.use(session({
 
 app.get(HOME, (req, res) => {
     const { query: { lang }, session: { userId, token, query } } = req
+    
 
     req.session.error = false
     // TODO make app multi-lang
+    
     lang ? req.session.lang = lang : req.session.lang = "en"
 
     try {
@@ -62,11 +64,11 @@ app.get(SEARCH, (req, res) => {
                 logic.retrieveUser(userId, token),
                 logic.searchDucks(userId, token, query)
             ])
-                .then(([user, ducks]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE)}${DuckResults(ducks)}`)))
+                .then(([user, ducks]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE, req.session.lang)}${DuckResults(ducks)}`)))
                 .catch(error => { throw error })
         else
             logic.searchDucks(undefined, undefined, query)
-                .then(ducks => res.send(Html(`${Header(undefined, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE)}${DuckResults(ducks)}`)))
+                .then(ducks => res.send(Html(`${Header(undefined, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE, req.session.lang)}${DuckResults(ducks)}`)))
     } catch (error) {
         throw error
     }
@@ -83,11 +85,11 @@ app.get(`${DETAIL}/:id`, (req, res) => {
                 logic.retrieveUser(userId, token),
                 logic.retrieveDuck(userId, token, duckId)
             ])
-                .then(([user, duck]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE)}${DuckDetail(duck)}`)))
+                .then(([user, duck]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE, req.session.lang)}${DuckDetail(duck)}`)))
                 .catch(error => { throw error })
         else
             logic.retrieveDuck(undefined, undefined, duckId)
-                .then(duck => res.send(Html(`${Header(undefined, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE)}${DuckDetail(duck)}`)))
+                .then(duck => res.send(Html(`${Header(undefined, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE, req.session.lang)}${DuckDetail(duck)}`)))
     } catch (error) {
         throw error
     }
@@ -102,46 +104,47 @@ app.get(FAVORITE, (req, res) => {
                 logic.retrieveUser(userId, token),
                 logic.retrieveFavDucks(userId, token)
             ])
-                .then(([user, ducks]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE)}${DuckResults(ducks)}`)))
+                .then(([user, ducks]) => res.send(Html(`${Header(user.name, query, SEARCH, SIGN_IN, SIGN_UP, SIGN_OUT, FAVORITE, req.session.lang)}${DuckResults(ducks)}`)))
                 .catch(error => { throw error })
-        else    res.redirect(SIGN_IN)
+        else res.redirect(SIGN_IN)
     } catch (error) {
         throw error
     }
 })
 
 app.get(SIGN_UP, (req, res) => {
-    const { session: { lang = 'en' } } = req
-
-    res.send(Html(Register(lang)))
+    const { session: { lang = 'en', error } } = req
+    res.send(Html(Register(lang, error)))
 })
 
 app.post(SIGN_UP, formBodyParser, (req, res) => {
     const { body } = req
-
     const { name, surname, email, password, repassword } = body
 
     try {
         logic.registerUser(name, surname, email, password, repassword)
             .then(() => res.send(Html(RegisterSuccess(SIGN_IN))))
-            .catch(error => { throw error })
+            .catch(error => {
+                req.session.error = error.message
+                res.redirect(SIGN_UP)
+            })
     } catch (error) {
-        throw error
+        req.session.error = error.message
+        res.redirect(SIGN_UP)
     }
 })
 
 app.get(SIGN_IN, (req, res) => {
-    const {session}=req
-    session.view= SIGN_IN
-    const{lang}=session
-    res.send(Html(Login(lang)))
+    const { session } = req
+    session.view = SIGN_IN
+    const { lang } = session
+    const{error}=session
+    res.send(Html(Login(lang, error)))
 })
 
 app.post(SIGN_IN, formBodyParser, (req, res) => {
     const { body, session } = req
-
     const { email, password } = body
-
     try {
         logic.authenticateUser(email, password)
             .then(({ id, token }) => {
@@ -150,37 +153,38 @@ app.post(SIGN_IN, formBodyParser, (req, res) => {
 
                 res.redirect(HOME)
             })
-            .catch(error => { throw error })
+            .catch(error => {
+                req.session.error=error.message
+                res.redirect(SIGN_IN)
+             })
     } catch (error) {
-        throw error
+        req.session.error=error.message
+        res.redirect(SIGN_IN)
     }
 })
 
 app.post(SIGN_OUT, (req, res) => {
     const { session } = req
-
     delete session.userId
     delete session.token
-
     res.redirect(HOME)
 })
 
 app.post(TOGGLE_FAV, formBodyParser, (req, res) => {
     const { body: { id }, session: { userId, token, query, view } } = req
 
-    
-
     if (userId && token)
         try {
             logic.toggleFavDuck(userId, token, id)
                 .then(() => {
-                if(view === 'search'){
-                    res.redirect(`${SEARCH}/?q=${query}`)
-                }else if(view === 'favorite'){
-                    res.redirect(`${FAVORITE}/?q=${query}`)
-                }else{
-                    res.redirect(`${DETAIL}/${id}`)
-                }}  )
+                    if (view === 'search') {
+                        res.redirect(`${SEARCH}/?q=${query}`)
+                    } else if (view === 'favorite') {
+                        res.redirect(`${FAVORITE}/?q=${query}`)
+                    } else {
+                        res.redirect(`${DETAIL}/${id}`)
+                    }
+                })
                 .catch(error => { throw error })
         } catch (error) {
             throw error
