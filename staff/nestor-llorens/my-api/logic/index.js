c531b2ce2e45e714186112e523f22b3a7534eb36
Nestor <nestor@pop-os.localdomain>
2019-08-20 21:11:17 +0200

    first commit

---
+++ staff/nestor-llorens/my-api/logic/index.js
@@ -0,0 +1,22 @@
+const validate = require('../utils/validate')
+
+module.exports = {
+
+    registerUser(name, surname, email, password, repassword) {
+        validate.string(name, 'name')
+        validate.string(surname, 'surname')
+        validate.string(email, 'username')
+        validate.email(email, 'username')
+        validate.string(password, 'password')
+        validate.string(repassword, 'password repeat')
+
+        if (password !== repassword) throw new Error('passwords do not match')
+
+        return this.__users__.findOne({ email })
+          .then(user => {
+              if (user) throw Error('Email is already registered')
+              
+              else this.__users__.insertOne({ name, surname, email, password})
+        }
+    )}
+}
