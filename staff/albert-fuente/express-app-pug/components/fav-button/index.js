3b8e41558ed17f34ccc738820f9a46aeeaca0efd
albert fuente homs <example@example.com>
2019-08-20 09:11:52 +0200

    added files

---
+++ staff/albert-fuente/express-app-pug/components/fav-button/index.js
@@ -0,0 +1,6 @@
+/* module.exports = function (id, selected) {
+    return `<form method="post" action="/toggle-favorite">
+            <input type="hidden" name="id" value="${id}" />
+            <button>${selected ? '💜' : '💔'}</button>
+        </form>`
+} */
