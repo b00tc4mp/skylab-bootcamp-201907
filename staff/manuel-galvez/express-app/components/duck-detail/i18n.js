c27a58ac70ef09af67d621091a02e8d50cbe77ba
lolo <lolo@lolo>
2019-08-22 17:34:07 +0200

    add last version express-app

---
+++ staff/manuel-galvez/express-app/components/duck-detail/i18n.js
@@ -0,0 +1,10 @@
+module.exports = {
+    en: { store: 'Go to store', goback: 'Go back'},
+    es: { store: 'Ir a la tienda', goback: 'Volver'},
+    ca: { store: 'Anar a la tenda', goback: 'Tornar enrere'},
+    fr: { store: 'Aller à la boutique', goback: 'Retourner'},
+}
+
+
+
+        
