82782286440240a8dd2dc074f02b8a37044e356b
lolo <lolo@lolo>
2019-08-23 10:35:01 +0200

    clean comments from index

--- staff/manuel-galvez/my-api/index.js
+++ staff/manuel-galvez/my-api/index.js
@@ -36,64 +36,6 @@ process.on('SIGINT', () => {
 })
         
 
-/*
-        app.get('/user/:id', (req, res) => {
-
-            const { params: { id }, headers: { authorization } } = req
-
-            const token = authorization.slice(authorization.indexOf(' ') + 1)
-
-            try {
-                jwt.verify(token, secret)
-
-                logic.retrieveUser(id)
-                    .then(user => res.json({ message: 'User retrieved successfully', user }))
-                    .catch(({ message }) => res.status(404).json({ error: message }))
-            } catch({ message }) {
-                res.status(404).json({ error: message })
-            }
-        })
-
-        app.delete('/user/:id', jsonBodyParser, (req, res) => {
-
-            const { params: { id }, headers: { authorization }, body: { email, password } } = req
-
-            const token = authorization.slice(authorization.indexOf(' ') + 1)
-
-            try {
-                jwt.verify(token, secret)
-
-                logic.unregisterUser(id, email, password)
-                    .then(() => res.json({ message: 'User unregistered successfully'}))
-                    .catch(({ message }) => res.status(404).json({ error: message }))
-            } catch({ message }) {
-                res.status(404).json({ error: message })
-            }
-        })
-
-
-        app.put('/user/:id', jsonBodyParser, (req, res) => {
-
-            const { params: { id }, headers: { authorization }, body } = req
-
-            const token = authorization.slice(authorization.indexOf(' ') + 1)
-
-            try {
-                jwt.verify(token, secret)
-
-                logic.updateUser(id, body)
-                    .then(() => res.json({ message: 'User updated successfully'}))
-                    .catch(({ message }) => res.status(400).json({ error: message }))
-            } catch({ message }) {
-                res.status(404).json({ error: message })
-            }
-        })
-
-
-    app.listen(port)
-    */
-
-
 
 
 
