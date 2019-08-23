702fa57e70f873e35486f1f0cba503621f925d31
CarolinaZLin <carolina.z.lin@gmail.com>
2019-08-23 09:57:28 +0200

    fix validate testing

--- staff/carolina-zhou/my-api/utils/validate/index.spec.js
+++ staff/carolina-zhou/my-api/utils/validate/index.spec.js
@@ -1,4 +1,5 @@
 const validate = require('.')
+const { expect } = require('chai')
 
 const {
     random
@@ -8,23 +9,23 @@ describe('logic - validate', () => {
     describe('string', () => {
         it('should succeed on correct string', () => {
             const result = validate.string('query', 'string')
-            expect(result).toBeUndefined()
+            expect(result).to.be.undefined
 
         })
         it('should fail on non correct string', () => {
             expect(() =>
-                validate.string(123, 'string')).toThrowError(Error, `string with value 123 is not a string`)
+                validate.string(123, 'string')).to.throw(Error, `string with value 123 is not a string`)
 
         })
         it('should fail on empty string', () => {
             expect(() =>
-                validate.string('', 'string')).toThrowError(Error, `string is empty or blank`)
+                validate.string('', 'string')).to.throw(Error, `string is empty or blank`)
 
         })
 
         it('should fail on empty string', () => {
             expect(() =>
-                validate.string('', 'string')).toThrowError(Error, `string is empty or blank`)
+                validate.string('', 'string')).to.throw(Error, `string is empty or blank`)
 
         })
 
@@ -32,12 +33,12 @@ describe('logic - validate', () => {
     describe('email', () => {
         it('should succeed on correct email', () => {
             const result = validate.email('John-' + random() + '@gmail.com', 'string')
-            expect(result).toBeUndefined()
+            expect(result).to.be.undefined
 
         })
         it('should fail on non correct email', () => {
             expect(() =>
-                validate.email(123, 'email')).toThrowError(Error, `email with value 123 is not a valid e-mail`)
+                validate.email(123, 'email')).to.throw(Error, `email with value 123 is not a valid e-mail`)
 
         })
 
@@ -46,12 +47,12 @@ describe('logic - validate', () => {
     describe('URL', () => {
             it('should succeed on correct URL', () => {
                 const result = validate.url('https://developer.mozilla.org/es/', 'string')
-                expect(result).toBeUndefined()
+                expect(result).to.be.undefined
 
             })
             it('should fail on non correct URL', () => {
                 expect(() =>
-                    validate.url(123, 'url')).toThrowError(Error, `url with value 123 is not a valid URL`)
+                    validate.url(123, 'url')).to.throw(Error, `url with value 123 is not a valid URL`)
 
             })
         })
