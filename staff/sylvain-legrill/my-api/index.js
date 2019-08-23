c713daae421c8cbb6413ef2c3eab4c804780727c
Sylvain <legrills@gmail.com>
2019-08-23 09:18:38 +0200

    divide logic per sub index files add routes . Final version

--- staff/sylvain-legrill/my-api/index.js
+++ staff/sylvain-legrill/my-api/index.js
@@ -1,109 +1,34 @@
+require('dotenv').config()
+
 const express = require('express')
-const bodyParser = require('body-parser')
+const data = require('./data')
+const { name, version } = require('./package')
 const logic = require('./logic')
-const { MongoClient } = require('mongodb')
-const jwt = require('jsonwebtoken')
+const routes = require('./routes')
 
+const { env: { PORT, DB_URL, DB_NAME } } = process
 
+let client
 
-//el useNewUrlParser creiem! que es un parametre per desactivar el default parser de mongo i usar el que li diem aki
-const client = new MongoClient('mongodb://localhost', { useNewUrlParser: true, useUnifiedTopology: true })
+data(DB_URL, DB_NAME)
+    .then(({ client: _client, db }) => {
+        client = _client
 
+        const users = db.collection('users')
 
-
-//secret es una password que te de coincidir aqui amb a la logica
-const secret = "uni dori teri cateri mata la veri veri viro"
-
-
-
-client.connect()
-    .then(() => {
-        debugger
-        const db = client.db('my-api')
-        const users =  db.collection('users')
         logic.__users__ = users
-        const { argv: [,, port = 7080] } = process
+
         const app = express()
-        const jsonBodyParser = bodyParser.json()
 
-        
-        app.post('/user', jsonBodyParser, (req, res) => {
-            const { body: { name, surname, email, password } } = req
-            try {
-                logic.registerUser(name, surname, email, password)
-                    .then(() => res.status(201).json({ message: 'user correctly registered' }))
-                    .catch(({ message }) => res.status(400).json({ error: message }))
-            } catch ({ message }) {
-                res.status(400).json({ error: message })
-            }
-        })
+        app.use('/api', routes)
 
-
-        app.post('/auth', jsonBodyParser, (req, res) => {
-            const { body: { email, password } } = req
-            try {
-                logic.authenticateUser(email, password)
-                    .then(id => {
-                        const token = jwt.sign({ sub: id }, secret)
-                        res.json({ message: 'user correctly authenticated', id, token })
-                    })
-                    .catch(({ message }) => res.status(401).json({ error: message }))
-            } catch ({ message }) {
-                res.status(401).json({ error: message })
-            }
-        })
-
-        // app.put('/user/:id', jsonBodyParser, (req, res) => {         //UPDATE-USER
-        //     const { params: { id }, headers: { authorization } } = req
-        //     const token = authorization.slice(authorization.indexOf(' ') + 1)
-        //     try {
-        //         jwt.verify(token, secret)
-        //         //const token = jwt.sign({ sub: id }, secret)
-        //         logic.updateUser(id, )
-        //             .then(id => {
-        //                 res.json({ message: `user with id ${id} correctly updated` })
-        //             })
-        //             .catch(({ message }) => res.status(400).json({ error: message }))
-        //     } catch ({ message }) {
-        //         res.status(400).json({ error: message })
-        //     }
-        // })
-
-
-        app.put('/user/:id', jsonBodyParser, (req, res) => {
-            debugger
-            const { body } = req
-            
-            const { params: { id }, headers: { authorization } } = req
-            const token = authorization.slice(authorization.indexOf(' ') + 1)
-            try {
-                jwt.verify(token, secret)
-                logic.updateUser(id, body)
-                    .then(response => {
-                        if(response.result.nModified == 0)
-                            res.json({ message: `User with id ${id} has not been updated`})
-                        else
-                            res.json({ message: `User with id ${id} correctly updated`})
-                        })
-                    .catch(({ message }) => res.status(404).json({ error: message }))
-            } catch ({ message }) {
-                res.status(404).json({ error: message })
-            }
-        
-        })
-        app.delete('/user/:id', jsonBodyParser, (req, res) => {
-        
-            const { params: { id },  headers: { authorization } } = req
-            const token = authorization.slice(authorization.indexOf(' ') + 1)
-            try {
-                jwt.verify(token, secret)
-                logic.removeUser(id)
-                    .then(()  => res.json({ message: 'User deleted'}))
-                    .catch(({ message }) => res.status(404).json({ error: message })) //mirar que posar
-            } catch ({ message }) {
-                res.status(404).json({ error: message })
-            }
-        })
-
-        app.listen(port)
+        app.listen(PORT, () => console.log(`${name} ${version} up and running on port ${PORT}`))
     })
+
+process.on('SIGINT', () => {
+    console.log(`\n${name} ${version} shutting down, disconnecting from db...`)
+
+    client.close()
+
+    process.exit(0)
+})
