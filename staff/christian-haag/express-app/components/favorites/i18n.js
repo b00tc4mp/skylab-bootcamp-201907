31eb7c15aa2a32c4ae7199b2dd6aa8da0be92649
vanhaaggen <vanhaaggen@gmail.com>
2019-08-23 11:14:51 +0200

    add express-app

---
+++ staff/christian-haag/express-app/components/favorites/i18n.js
@@ -0,0 +1,7 @@
+module.exports = {
+    en: { seccionTitle: 'Your favorite ducks' },
+    es: { seccionTitle: 'Tus patos favoritos' },
+    ca: { seccionTitle: 'Els teus ànecs preferits' },
+    de: { seccionTitle: 'Deine Lieblings Enten' },
+    kli: { seccionTitle: 'HochHom rur tel Qaj ' }
+}
---
+++ staff/christian-haag/express-app/components/register-success/i18n.js
@@ -0,0 +1,7 @@
+module.exports = {
+    en: { text: 'User successfully registered, you can now proceed to ', login: 'Login' },
+    es: { text: 'Usuario registrado correctamente, puede ', login: 'Acceder' },
+    ca: { text: 'Usuari registrat correctament, pot ', login: 'Accedir' },
+    de: { text: 'Benutzer erfolgreich registriert, Sie Können jetzt ', login: 'Einlogen' },
+    kli: { text: 'ghot\'e\' tuQ Suy qachmey, laH DaH ruch SoH ', login: 'naw\'' }
+}
