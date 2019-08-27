b7b428cd365622bbdc74557a3b90ecb0832e1f74
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-08-27 10:36:04 +0200

    update my-stuff-api, add register property

--- staff/manuel-barzi/my-stuff-api/0/data/models.js
+++ staff/manuel-barzi/my-stuff-api/0/data/models.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
-const { user, card, vehicle } = require('./schemas')
+const { user, card, vehicle, property } = require('./schemas')
 
 module.exports = {
     User: mongoose.model('User', user),
     Card: mongoose.model('Card', card),
-    Vehicle: mongoose.model('Vehicle', vehicle)
+    Vehicle: mongoose.model('Vehicle', vehicle),
+    Property: mongoose.model('Property', property)
 }
