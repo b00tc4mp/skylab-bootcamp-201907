d86879000df4e8a42dd738455923d4d9f2a2b584
dam0ne <dam0ne@hemwick>
2019-08-28 21:07:14 +0200

    small update

--- staff/jorge-garces/my-stuff-api/1/index.js
+++ staff/jorge-garces/my-stuff-api/1/index.js
@@ -11,22 +11,30 @@ const { env: { PORT, DB_URL } } = process
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
 
-        const app = express()    
+        const app = express()
+
+        app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            next()
+        })
 
         app.use('/api', routes)
 
 
-        app.listen(PORT, () => console.log(`${name} ${version} up and running on port ${PORT}`)) 
+
+
+        app.listen(PORT, () => console.log(`${name} ${version} up and running on port ${PORT}`))
     })
 
 process.on('SIGINT', () => {
     console.log(`\n${name} ${version} shutting down, disconnecting from db...`)
 
     mongoose.disconnect()
-    
+
     process.exit(0)
 })
-        
+
 
 
 
