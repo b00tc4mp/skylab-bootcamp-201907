a5c9beae87ea6827b7f0bbab094c282201a9c508
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-09-03 13:16:51 +0200

    update testing client side so to use data directly

--- staff/manuel-barzi/my-stuff-app/1/src/logic/authenticate-user/index.js
+++ staff/manuel-barzi/my-stuff-app/1/src/logic/authenticate-user/index.js
@@ -1,9 +1,12 @@
 // const { env: { REACT_APP_API_URL } } = process
+import { validate } from 'my-stuff-utils'
 
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL
 
 export default function (email, password) {
-    // validate fields
+    validate.string(email, 'e-mail')
+    validate.email(email, 'e-mail')
+    validate.string(password, 'password')
 
     return (async () => {
         const response = await fetch(`${REACT_APP_API_URL}/auth`, {
