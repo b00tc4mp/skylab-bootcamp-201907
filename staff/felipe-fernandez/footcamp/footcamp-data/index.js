1fd2b15ccd4a98e51d58be526a030ad10a92efb4
Felipe <felipefcor@gmail.com>
2019-09-03 12:17:54 +0200

    add database and package.json init

--- staff/felipe-fernandez/footcamp/footcamp-data/index.js
+++ staff/felipe-fernandez/footcamp/footcamp-data/index.js
@@ -1,9 +1,4 @@
-const mongoose = require('mongoose')
-const { user, league, team, player } = require('./schemas')
-
 module.exports = {
-    User: mongoose.model('User', user),
-    League: mongoose.model('League', league),
-    Team: mongoose.model('Team', team),
-    Player: mongoose.model('Player', player)
+    models: require('./models'),
+    database: require('./database')
 }
