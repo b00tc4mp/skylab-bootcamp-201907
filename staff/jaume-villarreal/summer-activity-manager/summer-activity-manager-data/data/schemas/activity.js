a956c50a84d22d5b7809a19330a5e30d4025c823
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-09-03 11:16:44 +0200

    init feature/sam-data branch

---
+++ staff/jaume-villarreal/summer-activity-manager/summer-activity-manager-data/data/schemas/activity.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const { Schema , Schema : { Types : { ObjectId } } } = mongoose
+
+module.exports = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    limit : Number
+})
