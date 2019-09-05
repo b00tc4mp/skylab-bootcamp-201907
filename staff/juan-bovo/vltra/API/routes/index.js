f4158dbd80d34ff3859b7358c01e35e7ad10b491
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-09-05 08:28:27 +0200

    added routes and also created files for posts and comments logic and test

--- staff/juan-bovo/vltra/API/routes/index.js
+++ staff/juan-bovo/vltra/API/routes/index.js
@@ -8,6 +8,16 @@ const retrieveUser = require('./user/retrieve-user')
 const updateUser = require('./user/update-user')
 const unregisterUser = require('./user/unregister-user')
 
+const createPost = require('./post/create-post')
+const retrievePost = require('./post/retrieve-post')
+const retrieveAllPosts = require('./post/retrieve-all-posts')
+//const retrieveUserPosts = require('./post/retrieve-user-posts')
+const deletePost = require('./post/delete-post')
+
+// const addComment = require('./comment/add-comment')
+// const retrievePostComments = require('./comment/retrieve-post-comments')
+// const deleteComment = require('./comment/delete-comment')
+
 
 const router = Router()
 const jsonBodyParser = bodyParser.json()
@@ -20,7 +30,15 @@ router.patch ('/users/:id', [tokenMiddleware, jsonBodyParser], updateUser)
 router.delete ('/users/:id', [tokenMiddleware, jsonBodyParser], unregisterUser)
 
 /* POST */
+router.post('/posts', jsonBodyParser, createPost)
+router.get('/posts/:id', [tokenMiddleware, jsonBodyParser], retrievePost)
+router.get('/posts/', [tokenMiddleware, jsonBodyParser], retrieveAllPosts)
+//router.get('users/:id/posts/:id', [tokenMiddleware, jsonBodyParser], retrieveUserPosts)
+router.delete ('/posts/:id', [tokenMiddleware, jsonBodyParser], deletePost)
 
 /* COMMENTS */
+// router.post('/posts/:id/comments', jsonBodyParser, addComment)
+// router.get('/posts/:id/comments/', [tokenMiddleware, jsonBodyParser], retrievePostComments)
+// router.delete ('/posts/:id/comments/:id', [tokenMiddleware, jsonBodyParser], deleteComment)
 
 module.exports = router
