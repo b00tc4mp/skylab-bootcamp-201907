768d90abe69baddd6aee880c30d5ae0348928b74
Anna <daeneris88@gmail.com>
2019-09-09 12:39:20 +0200

    call

---
+++ staff/anna-nolla/dogs-company/utils/call/index.js
@@ -0,0 +1,16 @@
+import validate from '../validate'
+
+function call(url, method = 'get', headers, body) {
+    validate.string(url, 'url')
+    // validate.url(url, 'url')
+    validate.string(method, 'method', true, ['get', 'post', 'put', 'patch', 'delete'])
+
+    return fetch(url, {
+        method,
+        headers,
+        body: JSON.stringify(body)
+    })
+        .then(res => res.json())
+}
+
+export default call
