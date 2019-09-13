eb149efd316fe024ebc0ff885cf509ca6e5d1e6f
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-09-13 19:49:32 +0200

    correct test

--- staff/anna-nolla/dogs-company/dog-company-app/src/logic/user/is-user-logged-in/index.spec.js
+++ staff/anna-nolla/dogs-company/dog-company-app/src/logic/user/is-user-logged-in/index.spec.js
@@ -1,5 +1,4 @@
-import logic from '..'
-
+import logic from '../../'
 
 describe('logic - is user logged in', () => {
     beforeEach(async () => {
@@ -7,7 +6,7 @@ describe('logic - is user logged in', () => {
     })
 
     it('should succeed on correct data', async () => {
-        const loggedIn = logic.userLoggedIn()
+        const loggedIn = logic.isUserLoggedIn()
         
         expect(typeof loggedIn).toBe('boolean')
         expect(loggedIn).toBeTruthy()
