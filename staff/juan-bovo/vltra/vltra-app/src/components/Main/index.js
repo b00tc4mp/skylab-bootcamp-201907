7fbbe2255ec141925d7d622ca1c1a4f93a0da974
Juan Pablo Bovo <juampi@MacBook-Pro-de-Juan.local>
2019-09-16 13:10:21 +0200

    minor css updates

--- staff/juan-bovo/vltra/vltra-app/src/components/Main/index.js
+++ staff/juan-bovo/vltra/vltra-app/src/components/Main/index.js
@@ -7,6 +7,7 @@ function Main(){
         <div className="mosaic-grid__container">
             {/* first block is different due testing overflow on grid :) */}
             <h3 className="mosaic-grid__title">La convulsión de los albinos</h3>
+            <button className="mosaic-grid__bookmark-button"><i class="far fa-bookmark"></i></button>
             <div className="mosaic-grid__preview">
             <p className="mosaic-grid__preview--content">Francisco es albino. Un día sufre una convulsión, cae al piso y al despertar sólo es capaz de pronunciar la palabra: cenáculo. Los médicos, desorientados, no pueden explicar el fenómeno. Más tarde, Horacio, otro albino, también sufre convulsiones. Al despertar sólo es capaz de pronunciar “abril”.  Muy pronto, todos los albinos del mundo sólo pueden pronunciar una palabra. El desafío será ordenar el mensaje antes de que sea demasiado tarde.
             </p>
