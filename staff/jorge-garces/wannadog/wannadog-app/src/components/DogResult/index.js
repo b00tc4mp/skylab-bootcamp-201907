83bc206d210394a6c09e4aa072796f2a07d551ae
dam0ne <dam0ne@hemwick>
2019-09-23 17:08:59 +0200

    last commit then cleanup

--- staff/jorge-garces/wannadog/wannadog-app/src/components/DogResult/index.js
+++ staff/jorge-garces/wannadog/wannadog-app/src/components/DogResult/index.js
@@ -6,8 +6,11 @@ export default withRouter(function DogResult({ dog }) {
     let { name, gender, id, image } = dog
     // gender === "male" ? gender = <i class="fas fa-mars"></i> : gender = <i class="fas fa-venus"></i>
 
-    return <li className="results__li">
+    return <>
         <a href={`/#/dog/${id}`}><img alt="dog-face" className="results__img" src={image}></img></a>
         <h3 className="results__name">{name}<span>{gender === 'Male' ? <i class="fas fa-mars"></i> : <i class="fas fa-venus"></i>}</span></h3>
-    </li>
+    </>
 })
+
+{/* <li className="results__li">
+</li> */}
